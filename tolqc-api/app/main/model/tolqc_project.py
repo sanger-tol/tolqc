ed75ce4a101523208c01daca0f6b246a67c42441
Kiernan Harding <kh16@sanger.ac.uk>
2021-11-18 18:27:57 +0000

    Updated interim datatypes

--- tolqc-api/app/main/model/tolqc_project.py
+++ tolqc-api/app/main/model/tolqc_project.py
@@ -7,9 +7,9 @@ from .base import Base, db
 
 class TolqcProject(Base):
     __tablename__ = "project"
-    project_id = db.Column(db.String(), primary_key=True)
+    project_id = db.Column(db.Integer(), primary_key=True)
     name = db.Column(db.String())
     hierarchy_name = db.Column(db.String())
     description = db.Column(db.String())
     lims_id = db.Column(db.String())
-    accession_id = db.Column(db.String())
+    accession_id = db.Column(db.Integer())
--- tolqc-api/app/main/model/tolqc_run.py
+++ tolqc-api/app/main/model/tolqc_run.py
@@ -8,12 +8,12 @@ from .base import Base, db
 class TolqcRun(Base):
     __tablename__ = "run"
     row_id = db.Column(db.Integer())
-    run_id = db.Column(db.String(), primary_key=True)
+    run_id = db.Column(db.Integer(), primary_key=True)
     name = db.Column(db.String())
     hierarchy_name = db.Column(db.String())
-    platform_id = db.Column(db.String())
-    centre_id = db.Column(db.String())
-    lims_id = db.Column(db.String())
+    platform_id = db.Column(db.Integer())
+    centre_id = db.Column(db.Integer())
+    lims_id = db.Column(db.Integer())
     element = db.Column(db.String())
-    changed = db.Column(db.String())
-    current = db.Column(db.String())
+    changed = db.Column(db.DateTime())
+    current = db.Column(db.DateTime())
