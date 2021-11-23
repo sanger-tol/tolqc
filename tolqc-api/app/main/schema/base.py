57f030400db9ec0ad5b00e1426c870baf158866a
Edward Moulsdale <em29@sanger.ac.uk>
2021-11-23 13:46:51 +0000

    Base (detail) schema implemented

--- tolqc-api/app/main/schema/base.py
+++ tolqc-api/app/main/schema/base.py
@@ -5,15 +5,47 @@
 from main.ma import ma
 
 
-# every child's Meta class must have a type_ and id attribute
+# every child's Meta class must have a an id attribute
+class IdNotSpecifiedException(Exception):
+    def __init__(self, cls):
+        self._cls = cls
+
+    def __repr__(self):
+        return "The schema class for model '%s' does not have a specified id" \
+            % self._cls.Meta.model.__tablename__
+
+
+# every child's Meta class must have a an type attribute
+class TypeNotSpecifiedException(Exception):
+    def __init__(self, cls):
+        self._cls = cls
+
+    def __repr__(self):
+        return "The schema class for model '%s' does not have a specified type_" \
+            % self._cls.Meta.model.__tablename__
+
+
 class BaseSchema(ma.SQLAlchemyAutoSchema):
+    @classmethod
+    def _get_type(cls):
+        type_ = cls.Meta.__dict__.get('type_')
+        if type_ is None:
+            raise TypeNotSpecifiedException(cls)
+        return type_
+
+    @classmethod
+    def _get_id(cls):
+        id = cls.Meta.__dict__.get('id')
+        if id is None:
+            raise IdNotSpecifiedException(cls)
+        return id
+
     def dump(self, *args, **kwargs):
         data = super().dump(*args, **kwargs)
-        meta = self.Meta()
         return {
             "data": {
-                "type": meta.type_,
-                "id": data[meta.id],
+                "type": self._get_type(),
+                "id": data[self._get_id()],
                 "attributes": data,
             }
         }
