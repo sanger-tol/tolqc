22ffd97af988710f73a5127fba5f088877a92ba1
Edward Moulsdale <em29@sanger.ac.uk>
2021-11-29 13:50:40 +0000

    Added BaseResource

--- tolqc-api/app/main/resource/base.py
+++ tolqc-api/app/main/resource/base.py
@@ -2,11 +2,46 @@
 #
 # SPDX-License-Identifier: MIT
 
-from flask_restx import Namespace
+from flask_restx import Namespace, Resource
+
+from main.schema import RowDoesNotExistException
 
 
 class BaseNamespace(Namespace):
-    """Wrapper for flask-restxNamespace, that always validates
+    """Wrapper for flask-restx's Namespace - always validates
     input using Marshmallow"""
     def __init__(self, *args, **kwargs):
         super().__init__(*args, validate=True, **kwargs)
+
+
+class BaseDetailResource(Resource):
+    """Wrapper for flask-restx's Resource - provides default
+    implementations of GET, DELETE, PUT methods for a detail
+    resource
+    
+    Requires class/static variables request_schema and
+    response_schema to be declared in inherited classes, and a
+    name to be declared for errors
+    """
+    name = 'base'
+
+    def _404_error(self, id):
+        return {
+            "error": f"{self.name} with {id} not found."
+        }, 404
+
+    def _get_by_id(self, id):
+        try:
+            model = self.response_schema.get_by_id(id)
+            return model, 200
+        except RowDoesNotExistException:
+            return self._404_error(id)
+    
+    # TODO add auth
+    # TODO add deletion conflict exception
+    def _delete_by_id(self, id):
+        try:
+            self.response_schema.delete_by_id(id)
+            return {}, 204
+        except RowDoesNotExistException:
+            return self._404_error(id)
--- tolqc-api/app/main/resource/tolqc_centre_resource.py
+++ tolqc-api/app/main/resource/tolqc_centre_resource.py
@@ -4,10 +4,11 @@
 
 from flask_restx import Resource
 
-from .base import BaseNamespace
+from .base import BaseNamespace, BaseDetailResource
 from main.schema import TolqcCentreRequestSchema, \
-                        TolqcCentreResponseSchema, \
-                        RowDoesNotExistException
+                        TolqcCentreResponseSchema
+
+# TODO implement list resources (not just detail)
 
 centre_namespace = BaseNamespace(
     'centre',
@@ -28,8 +29,10 @@ centre_request_schema = TolqcCentreRequestSchema()
 centre_response_schema = TolqcCentreResponseSchema()
 
 
-class TolqcCentreDetailResource(Resource):
+class TolqcCentreDetailResource(BaseDetailResource):
     name = 'centre'
+    request_schema = TolqcCentreRequestSchema()
+    response_schema = TolqcCentreResponseSchema()
 
     @centre_namespace.response(
         200,
@@ -41,13 +44,7 @@ class TolqcCentreDetailResource(Resource):
         description='Not Found'
     )
     def get(self, id):
-        try:
-            centre = centre_response_schema.get_by_id(id)
-            return centre, 200
-        except RowDoesNotExistException:
-            return {
-                "error": f"Centre with {id} not found."
-            }, 404
+        return self._get_by_id(id)
     
     @centre_namespace.response(
         204,
@@ -57,16 +54,8 @@ class TolqcCentreDetailResource(Resource):
         404,
         description='Not Found'
     )
-    # TODO add auth
-    # TODO add deletion conflict exception
     def delete(self, id):
-        try:
-            centre_response_schema.delete_by_id(id)
-            return 204
-        except RowDoesNotExistException:
-            return {
-                "error": f"Centre with {id} not found."
-            }, 404
+        return self._delete_by_id(id)
 
 
 class TolqcCentreListResource(Resource):
