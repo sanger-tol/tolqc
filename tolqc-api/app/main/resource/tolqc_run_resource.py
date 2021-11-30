327e501ec02d179642e61245924c4a68eeffbaf9
Edward Moulsdale <em29@sanger.ac.uk>
2021-11-30 10:20:11 +0000

    Added run

--- tolqc-api/app/main/resource/tolqc_run_resource.py
+++ tolqc-api/app/main/resource/tolqc_run_resource.py
@@ -2,9 +2,11 @@
 #
 # SPDX-License-Identifier: MIT
 
-from flask_restx import Resource
+from flask_restx import Resource, namespace
 
-from .base import BaseNamespace
+from .base import BaseNamespace, BaseDetailResource
+from main.schema import TolqcRunRequestSchema, \
+                        TolqcRunResponseSchema
 
 
 run_namespace = BaseNamespace(
@@ -12,13 +14,89 @@ run_namespace = BaseNamespace(
     description='ToLQC-Run related methods',
 )
 
+run_response_model = run_namespace.schema_model(
+    'Run Response',
+    TolqcRunResponseSchema.to_schema_model_dict()
+)
 
-class TolqcRunDetailResource(Resource):
+run_post_model = run_namespace.model(
+    'Run POST Request',
+    TolqcRunRequestSchema.to_post_model_dict()
+)
+
+run_put_model = run_namespace.model(
+    'Run PUT Request',
+    TolqcRunRequestSchema.to_put_model_dict()
+)
+
+
+class TolqcRunDetailResource(BaseDetailResource):
     name = "run"
+    namespace = run_namespace
+    request_schema = TolqcRunRequestSchema()
+    response_schema = TolqcRunResponseSchema()
+
+    @run_namespace.response(
+        200,
+        'Success',
+        run_response_model
+    )
+    @run_namespace.response(
+        404,
+        'Not Found'
+    )
+    def get(self, id):
+        return self._get_by_id(id)
+    
+    @run_namespace.expect(run_put_model)
+    @run_namespace.response(
+        200,
+        'Success',
+        run_response_model
+    )
+    @run_namespace.response(
+        400,
+        'Bad Request'
+    )
+    @run_namespace.response(
+        404,
+        'Not Found'
+    )
+    def put(self, id):
+        return self._put_by_id(id)
+    
+    @run_namespace.response(
+        204,
+        'Success'
+    )
+    @run_namespace.response(
+        404,
+        'Not Found'
+    )
+    def delete(self, id):
+        return self._delete_by_id(id)
 
 
 class TolqcRunListResource(Resource):
     name = "runs"
+    
+    @run_namespace.expect(run_post_model)
+    @run_namespace.response(
+        200,
+        description='Success',
+        model=run_response_model,
+    )
+    @run_namespace.response(
+        400,
+        description='Error'
+    )
+    def post(self):
+        data = run_namespace.payload
+        return TolqcRunResponseSchema().dump(
+            TolqcRunRequestSchema().create(
+                data
+            )
+        ), 200
 
 
 run_namespace.add_resource(TolqcRunDetailResource, '/<int:id>')
