202d9fb5b0fd70ec0f5fd8c86a471ebbc94b44af
Edward Moulsdale <em29@sanger.ac.uk>
2021-12-01 16:02:32 +0000

    Refactored, added 404 tests

---
+++ tolqc-api/app/test/core/models/__init__.py
@@ -0,0 +1,8 @@
+# SPDX-FileCopyrightText: 2021 Genome Research Ltd.
+#
+# SPDX-License-Identifier: MIT
+
+from .models import ModelRelationshipA, \
+                    ModelRelationshipB, \
+                    ModelWithNullableColumn, \
+                    ModelWithNonNullableColumn
