d01f2de03ae5e8a5a948218ff4c7b66806cfe6a2
Edward Moulsdale <em29@sanger.ac.uk>
2021-12-02 09:32:11 +0000

    Fiished id given test class

--- tolqc-api/app/test/core/resources/test_list_resource_id_given.py
+++ tolqc-api/app/test/core/resources/test_list_resource_id_given.py
@@ -21,7 +21,29 @@ class TestListResourceIdGiven400(BaseTestCase):
         )
     
     def test_C_id_given_400_post(self):
-        pass
+        response = self.client.open(
+            '/api/v1/C',
+            method='POST',
+            json={
+                "id": 9999,
+                "nullable_column": "doesn't matter"
+            }
+        )
+        self.assert400(
+            response,
+            f'Response body is : {response.data.decode("utf-8")}'
+        )
 
     def test_D_id_given_400_post(self):
-        pass
+        response = self.client.open(
+            '/api/v1/D',
+            method='POST',
+            json={
+                "id": 9999,
+                "non_nullable_column": "Not at all"
+            }
+        )
+        self.assert400(
+            response,
+            f'Response body is : {response.data.decode("utf-8")}'
+        )
