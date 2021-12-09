c51650a6f1e7ff3d109019198efb02438d9ec450
Edward Moulsdale <em29@sanger.ac.uk>
2021-12-09 14:56:39 +0000

    9 left

--- tolqc-api/app/test/core/test_cases/test_column_nullability_post.py
+++ tolqc-api/app/test/core/test_cases/test_column_nullability_post.py
@@ -6,28 +6,34 @@ from test.core import BaseTestCase
 
 
 class TestColumnNullabilityPost(BaseTestCase):
-    def test_nullable_column_omitted_post_C_200(self):
+    def test_nullable_column_omitted_post_C_no_error(self):
         response = self.client.open(
             '/api/v1/C',
             method='POST',
-            json={
+            json=[{
                 "other_column": "no matter"
-            }
+            }]
         )
         self.assert200(
             response,
             f'Response body is : {response.data.decode("utf-8")}'
         )
+        errors = response.json['meta']['errors']
+        self.assertEqual(len(errors), 1)
+        self.assertEqual(errors[0], None)
 
-    def test_non_nullable_column_omitted_post_D_400(self):
+    def test_non_nullable_column_omitted_post_D_error(self):
         response = self.client.open(
             '/api/v1/D',
             method='POST',
-            json={
+            json=[{
                 "other_column": "no matter"
-            }
+            }]
         )
-        self.assert400(
+        self.assert200(
             response,
             f'Response body is : {response.data.decode("utf-8")}'
         )
+        errors = response.json['meta']['errors']
+        self.assertEqual(len(errors), 1)
+        self.assertNotEqual(errors[0], None)
