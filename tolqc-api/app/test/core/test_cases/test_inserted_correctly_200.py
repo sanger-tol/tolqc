517a5cd2afec15fa4a0ddd6feb96a10c88420b7e
Edward Moulsdale <em29@sanger.ac.uk>
2021-12-09 14:03:04 +0000

    Another test passes

--- tolqc-api/app/test/core/test_cases/test_inserted_correctly_200.py
+++ tolqc-api/app/test/core/test_cases/test_inserted_correctly_200.py
@@ -8,8 +8,8 @@ from test.core.models import ModelRelationshipB, \
                              ModelWithNonNullableColumn
 
 # TODO check multiple inserts
-class TestInsertedCorrectly200(BaseTestCase):
-    def test_inserted_correctly_B_200(self):
+class TestInsertedCorrectly(BaseTestCase):
+    def test_inserted_correctly_B(self):
         a_id = 90
         self.add_A(id=a_id)
 
@@ -31,24 +31,32 @@ class TestInsertedCorrectly200(BaseTestCase):
         self.assertEqual(inserted.a_id, a_id)
 
     def test_inserted_correctly_C_200(self):
-        other_column_string = 'this is a test, nice'
-
         response = self.client.open(
             '/api/v1/C',
             method='POST',
-            json={
-                'other_column': other_column_string
-            }
+            json=[{
+                'other_column': 'this is a test, nice'
+            }, {
+                'other_column': 'another test',
+                'nullable_column': 'nothing to see here'
+            }]
         )
         self.assert200(
             response,
             f'Response body is : {response.data.decode("utf-8")}'
         )
-        id = response.json['data']['id']
 
-        inserted = ModelWithNullableColumn.find_by_id(id)
-        self.assertEqual(inserted.nullable_column, None)
-        self.assertEqual(inserted.other_column, other_column_string)
+        self.assertEqual(response.json['meta']['errors'], [None, None])
+
+        id_0 = response.json['data'][0]['id']
+        inserted_0 = ModelWithNullableColumn.find_by_id(id_0)
+        self.assertEqual(inserted_0.nullable_column, None)
+        self.assertEqual(inserted_0.other_column, 'this is a test, nice')
+
+        id_1 = response.json['data'][1]['id']
+        inserted_1 = ModelWithNullableColumn.find_by_id(id_1)
+        self.assertEqual(inserted_1.nullable_column, 'nothing to see here')
+        self.assertEqual(inserted_1.other_column, 'another test')
 
     def test_inserted_correctly_D_200(self):
         other_column_string = 'this is also a test'
