11d494a794f846a98dbcb457e746c561100f2551
Edward Moulsdale <em29@sanger.ac.uk>
2021-12-09 14:04:30 +0000

    Yet another test passing

--- tolqc-api/app/test/core/test_cases/test_inserted_correctly_200.py
+++ tolqc-api/app/test/core/test_cases/test_inserted_correctly_200.py
@@ -30,7 +30,7 @@ class TestInsertedCorrectly(BaseTestCase):
         inserted = ModelRelationshipB.find_by_id(id)
         self.assertEqual(inserted.a_id, a_id)
 
-    def test_inserted_correctly_C_200(self):
+    def test_multiple_inserted_correctly_C_200(self):
         response = self.client.open(
             '/api/v1/C',
             method='POST',
@@ -65,20 +65,25 @@ class TestInsertedCorrectly(BaseTestCase):
         response = self.client.open(
             '/api/v1/D',
             method='POST',
-            json={
+            json=[{
                 'other_column': other_column_string,
                 'non_nullable_column': non_nullable_string
-            }
+            }]
         )
         self.assert200(
             response,
             f'Response body is : {response.data.decode("utf-8")}'
         )
-        id = response.json['data']['id']
 
+        self.assertEqual(response.json['meta']['errors'], [None])
+
+        id = response.json['data'][0]['id']
         inserted = ModelWithNonNullableColumn.find_by_id(id)
         self.assertEqual(
             inserted.non_nullable_column,
             non_nullable_string
         )
         self.assertEqual(inserted.other_column, other_column_string)
+    
+    def partial_insertion_success_F(self):
+        pass
