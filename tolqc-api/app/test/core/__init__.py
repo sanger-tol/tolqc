7eda51b20f8924ea11e7a070060826b91e21dae3
Edward Moulsdale <em29@sanger.ac.uk>
2021-12-09 15:47:38 +0000

    2. 1. GO

--- tolqc-api/app/test/core/__init__.py
+++ tolqc-api/app/test/core/__init__.py
@@ -84,3 +84,13 @@ class BaseTestCase(TestCase):
 
     def add_F(self, **kwargs):
         self._add_model_instance(ModelWithExtField, **kwargs)
+
+    def to_json_api(self, id, type, attributes):
+        return {
+            'data': {
+                'type': type,
+                'id': id,
+                'attributes': attributes
+            }
+        }
+
