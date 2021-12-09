692d6ba22cfcdd293f1335314647bb984edc0d28
Kiernan Harding <kh16@sanger.ac.uk>
2021-12-09 11:02:15 +0000

    Tidied up auth decorator

--- tolqc-api/app/main/resource/auth.py
+++ tolqc-api/app/main/resource/auth.py
@@ -4,27 +4,34 @@
 
 from main import model
 from functools import wraps
-from flask import request
-from main.custom_exception import CustomException
+from flask import request, abort
+from flask_restx import Namespace
 
 
 def check_key_valid(api_key):
     user_id = model.TolqcUser.get_user_infos_by_api_key(api_key)
     if not user_id:
-        raise CustomException(401, "User does not exist")
+        abort(401, "User does not exist")
     return user_id
 
 
-def require_auth():
+def auth_dec():
     def warp_decorator(function):
         @wraps(function)
         def decorated(*args, **kwargs):
             api_key = request.headers.get('Authorization')
             if not api_key:
-                raise CustomException(401, "Api key is missing")
+                abort(401, "Api key is missing")
             user = check_key_valid(api_key)
             request_data = dict(user_id=user.id, **kwargs)
             return function(*args, request_data)
-
         return decorated
     return warp_decorator
+
+def auth(namespace:Namespace):
+    decs = (namespace.doc(security=['ApiKeyAuth']), auth_dec())
+    def deco(func):
+        for dec in reversed(decs):
+            func = dec(func)
+        return func
+    return deco
