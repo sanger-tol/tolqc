6b04156ffa208d7f73c2411b53add4bbe6f0d89a
Edward Moulsdale <em29@sanger.ac.uk>
2021-12-13 16:04:33 +0000

    More error messaging

--- tolqc-api/app/main/service/base.py
+++ tolqc-api/app/main/service/base.py
@@ -9,18 +9,45 @@ from flask import Response
 
 class BaseService:
     @classmethod
+    def _split_error_components(cls, errors):
+        titles = [e.get('title', None) for e in errors]
+        codes = [e.get('code', None) for e in errors]
+        details = [e.get('detail', None) for e in errors]
+    
+        return titles, codes, details
+    
+    @classmethod
+    def _format_error(cls, title, code, detail):
+        return {
+            "title": title if title else "Internal Server Error",
+            "code": code if code else 500,
+            "detail": detail if detail else "An unknown error occured"
+        }
+
+    @classmethod
+    def custom_post_partial_error(cls, errors=[]):
+        titles, codes, details = cls._split_error_components(errors)
+
+        error_messages = [
+            None 
+            if t is None and c is None and d is None
+            else cls._format_error(t, c, d)
+            for (t, c, d) in zip(titles, codes, details)
+        ]
+
+        return {
+            'errors': error_messages
+        }
+
+    @classmethod
     def custom_error(cls, status_code=500, errors=[]):
         """Expects a list of dicts, with keys 'title', 'code', and 'detail"""
 
-        error_titles = [e.get('title', None) for e in errors]
-        error_codes = [e.get('code', None) for e in errors]
-        error_details = [e.get('detail', None) for e in errors]
-
-        error_messages = [{
-            "title": t if t else "Internal Server Error",
-            "code": c if c else 500,
-            "detail": d if d else "An unknown error occured"
-        } for (t, c, d) in zip(error_titles, error_codes, error_details)]
+        titles, codes, details = cls._split_error_components(errors)
+        error_messages = [
+            cls._format_error(t, c, d)
+            for (t, c, d) in zip(titles, codes, details)
+        ]
 
         response = {
             'errors': error_messages
