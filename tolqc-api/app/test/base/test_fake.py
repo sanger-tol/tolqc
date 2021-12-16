58a2b638420be4dd494897c918525c65cffbeaa4
Edward Moulsdale <em29@sanger.ac.uk>
2021-12-16 12:54:34 +0000

    Repaired tests in preparation

---
+++ tolqc-api/app/test/base/test_fake.py
@@ -0,0 +1,10 @@
+# SPDX-FileCopyrightText: 2021 Genome Research Ltd.
+#
+# SPDX-License-Identifier: MIT
+
+from unittest import TestCase
+
+
+class TestFake(TestCase):
+    def test_fake(self):
+        self.assertTrue(True)
