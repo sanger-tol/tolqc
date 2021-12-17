be4138e7056b0267eef2d361818f12156f7f7da4
Edward Moulsdale <em29@sanger.ac.uk>
2021-12-17 10:06:25 +0000

    Fully refactored :)

--- tolqc-api/app/main/resource/base.py
+++ tolqc-api/app/main/resource/base.py
@@ -6,26 +6,34 @@ from flask_restx import Resource
 
 from main.auth import auth
 
-def _compose_decorators(function, *decorators):
+
+def _compose_decorators(function, decorators):
     decorators = reversed(decorators)
     func = function.__func__
     for d in decorators:
         func = d(func)
     return func
-            
 
-def _document_list_resource(cls):
+
+def _get_api_swagger(cls):
     swagger = cls.Meta.swagger
     api = swagger.api
+    return api, swagger
 
-    #cls.post = api.expect(swagger.post_request_model)(cls.post.__func__)
+
+def _document_post(cls):
+    api, swagger = _get_api_swagger(cls)
     decorators = (
         api.expect(swagger.post_request_model),
         api.response(201, description="Created"),
         auth(api)
     )
-    cls.post = _compose_decorators(cls.post, *decorators)
+    cls.post = _compose_decorators(cls.post, decorators)
+            
 
+def _document_list_resource(cls):
+    api, _ = _get_api_swagger(cls)    
+    _document_post(cls)
     return api.route('')(cls)
 
 
@@ -33,7 +41,6 @@ def document_resource(cls):
     if cls.is_list_resource():
         return _document_list_resource(cls)
         
-
 
 class BaseListResource(Resource):        
     @classmethod
