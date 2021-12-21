db6466697ec0b9691ba381c4b0606f694e5db1cd
Edward Moulsdale <em29@sanger.ac.uk>
2021-12-21 13:41:09 +0000

    Re-added base-test classes

---
+++ tolqc-api/app/test/base/models.py
@@ -0,0 +1,54 @@
+# SPDX-FileCopyrightText: 2021 Genome Research Ltd.
+#
+# SPDX-License-Identifier: MIT
+
+from main.model.base import Base, db, ExtColumn
+
+
+class ModelRelationshipA(Base):
+    __tablename__ = 'test_A'
+    # the variable below is necessary on every test model!
+    __table_args__ = {'extend_existing': True}
+    id = db.Column(db.Integer, primary_key=True)
+    test_B = db.relationship('ModelRelationshipB', back_populates='test_A')
+
+
+class ModelRelationshipB(Base):
+    __tablename__ = 'test_B'
+    __table_args__ = {'extend_existing': True}
+    id = db.Column(db.Integer, primary_key=True)
+    a_id = db.Column(db.Integer, db.ForeignKey("test_A.id"), nullable=False)
+    test_A = db.relationship(ModelRelationshipA, back_populates='test_B', foreign_keys=[a_id])
+    test_E = db.relationship('ModelRelationshipE', back_populates='test_B')
+
+
+class ModelWithNullableColumn(Base):
+    __tablename__ = 'test_C'
+    __table_args__ = {'extend_existing': True}
+    id = db.Column(db.Integer, primary_key=True)
+    nullable_column = db.Column(db.String, nullable=True)
+    other_column = db.Column(db.String, nullable=True)
+
+
+class ModelWithNonNullableColumn(Base):
+    __tablename__ = 'test_D'
+    __table_args__ = {'extend_existing': True}
+    id = db.Column(db.Integer, primary_key=True)
+    non_nullable_column = db.Column(db.String, nullable=False)
+    other_column = db.Column(db.String, nullable=True)
+
+
+class ModelRelationshipE(Base):
+    __tablename__ = 'test_E'
+    __table_args__ = {'extend_existing': True}
+    id = db.Column(db.Integer, primary_key=True)
+    b_id = db.Column(db.Integer, db.ForeignKey("test_B.id"), nullable=False)
+    test_B = db.relationship(ModelRelationshipB, back_populates='test_E', foreign_keys=[b_id])
+
+
+class ModelWithExtField(Base):
+    __tablename__ = 'test_F'
+    __table_args__ = {'extend_existing': True}
+    id = db.Column(db.Integer, primary_key=True)
+    ext = ExtColumn()
+    other_column = db.Column(db.String, nullable=True)
---
+++ tolqc-api/app/test/base/schemas.py
@@ -0,0 +1,52 @@
+# SPDX-FileCopyrightText: 2021 Genome Research Ltd.
+#
+# SPDX-License-Identifier: MIT
+
+from main.schema.base import BaseSchema, BaseMeta, \
+                             BaseExtSchema, BaseExtMeta, \
+                             setup_schema
+
+from test.base.models import ModelRelationshipB, \
+                             ModelWithNullableColumn, \
+                             ModelWithNonNullableColumn, \
+                             ModelWithExtField
+
+
+class B_Meta(BaseMeta):
+    type_ = 'B'
+    model = ModelRelationshipB
+
+
+@setup_schema
+class B_Schema(BaseSchema):
+    Meta = B_Meta
+
+
+class C_Meta(BaseMeta):
+    type_ = 'C'
+    model = ModelWithNullableColumn
+
+
+@setup_schema
+class C_Schema(BaseSchema):
+    Meta = C_Meta
+
+
+class D_Meta(BaseMeta):
+    type_ = 'D'
+    model = ModelWithNonNullableColumn
+
+
+@setup_schema
+class D_Schema(BaseSchema):
+    Meta = D_Meta
+
+
+class F_Meta(BaseExtMeta):
+    type_ = 'F'
+    model = ModelWithExtField
+
+
+@setup_schema
+class F_Schema(BaseExtSchema):
+    Meta = F_Meta
