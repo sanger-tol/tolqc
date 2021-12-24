87d6fa410d7fbdc52e4f5d75f0361e2d3c91f32b
Edward Moulsdale <em29@sanger.ac.uk>
2021-12-24 12:01:19 +0000

    Yay, more tests!

--- tolqc-api/app/test/base/tests/test_ext_fields.py
+++ tolqc-api/app/test/base/tests/test_ext_fields.py
@@ -20,7 +20,7 @@ class TestExtraFieldsInRequestBody(BaseTestCase):
             True
         )
 
-    def test_extra_fields_post_F_no_error(self):
+    def test_extra_fields_post_F_201(self):
         extra_fields = {
             "extra_field": "superfluity",
             "another_ext": "Yet another extra field"
@@ -61,3 +61,38 @@ class TestExtraFieldsInRequestBody(BaseTestCase):
         )
         F_instance = F_ModelWithExtField.find_by_id(id)
         self.assertEqual(F_instance.ext, extra_fields)
+    
+    def test_no_extra_fields_post_F_201(self):
+        response = self.client.open(
+            '/api/v1/F',
+            method='POST',
+            json={
+                'data': {
+                    'type': 'F',
+                    'attributes': {
+                        'other_column': 'nice test'
+                    }
+                }
+            },
+            headers=self._get_api_key()
+        )
+        self.assert201(response)
+    
+        id = response.json['data']['id']
+        self.assertEqual(
+            response.json,
+            {
+                'data': {
+                    'type': 'F',
+                    'attributes': {
+                        'other_column': 'nice test'
+                    },
+                    'id': id,
+                    'meta': {
+                        'ext': {}
+                    }
+                }
+            }
+        )
+        F_instance = F_ModelWithExtField.find_by_id(id)
+        self.assertEqual(F_instance.ext, {})
