1aaf7fdaa52c97b19d5f8e01b4f3aacf3d76293e
Edward Moulsdale <em29@sanger.ac.uk>
2022-01-18 13:11:33 +0000

    Many more test

--- tolqc-api/app/test/base/tests/test_malformed_filter_string.py
+++ tolqc-api/app/test/base/tests/test_malformed_filter_string.py
@@ -18,5 +18,35 @@ class TestMalformedFilterString(BaseTestCase):
             f'Response body is : {response.data.decode("utf-8")}'
         )
 
+        # opening, but not closing
+        response = self.client.open(
+            '/api/v1/G?filter=[string_column=="test, yet again"',
+            method='GET'
+        )
+        self.assert400(
+            response,
+            f'Response body is : {response.data.decode("utf-8")}'
+        )
+
+        # closing, but not opening
+        response = self.client.open(
+            '/api/v1/G?filter=string_column=="test, yet again"]',
+            method='GET'
+        )
+        self.assert400(
+            response,
+            f'Response body is : {response.data.decode("utf-8")}'
+        )
+
     def test_bad_single_equals_filter_G_400(self):
-        pass
+        self.add_G(id=34788, string_column='tests are fun!!')
+
+        # single instead of double equals sign
+        response = self.client.open(
+            '/api/v1/G?filter=[string_column="tests are fun!!"]',
+            method='GET'
+        )
+        self.assert400(
+            response,
+            f'Response body is : {response.data.decode("utf-8")}'
+        )
