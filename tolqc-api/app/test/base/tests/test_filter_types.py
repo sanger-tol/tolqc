7a4c37958b5c545e0e7aaaf847d2c915cd1b67d6
Edward Moulsdale <em29@sanger.ac.uk>
2022-01-18 10:52:37 +0000

    Wired up G_Model

---
+++ tolqc-api/app/test/base/tests/test_filter_types.py
@@ -0,0 +1,10 @@
+# SPDX-FileCopyrightText: 2021 Genome Research Ltd.
+#
+# SPDX-License-Identifier: MIT
+
+from test.base import BaseTestCase
+
+
+class TestFilterTypes(BaseTestCase):
+    def test_string_bad_delimiter_list_get_G_400(self):
+        pass
