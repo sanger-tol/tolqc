eff0beaa918004c706a355082b58982e446c9a71
Edward Moulsdale <em29@sanger.ac.uk>
2022-01-25 12:09:55 +0000

    Updated tests

--- tolqc-api/app/test/base/resources.py
+++ tolqc-api/app/test/base/resources.py
@@ -3,7 +3,7 @@
 # SPDX-License-Identifier: MIT
 
 
-from main.resource.base import BaseListResource, BaseDetailResource, setup_resource
+from main.resource.base import BaseResource, setup_resource
 
 from test.base.services import B_Service, C_Service, D_Service, F_Service, G_Service
 from test.base.swaggers import B_Swagger, C_Swagger, D_Swagger, F_Swagger, G_Swagger
@@ -16,71 +16,36 @@ api_F = F_Swagger.api
 api_G = G_Swagger.api
 
 
-class B_ResourceMeta:
-    service = B_Service
-    swagger = B_Swagger
+@setup_resource
+class B_Resource(BaseResource):
+    class Meta:
+        service = B_Service
+        swagger = B_Swagger
 
 
 @setup_resource
-class B_DetailResource(BaseDetailResource):
-    Meta = B_ResourceMeta
+class C_Resource(BaseResource):
+    class Meta:
+        service = C_Service
+        swagger = C_Swagger
 
 
 @setup_resource
-class B_ListResource(BaseListResource):
-    Meta = B_ResourceMeta
-
-
-class C_ResourceMeta:
-    service = C_Service
-    swagger = C_Swagger
+class D_Resource(BaseResource):
+    class Meta:
+        service = D_Service
+        swagger = D_Swagger
 
 
 @setup_resource
-class C_DetailResource(BaseDetailResource):
-    Meta = C_ResourceMeta
+class F_Resource(BaseResource):
+    class Meta:
+        service = F_Service
+        swagger = F_Swagger
 
 
 @setup_resource
-class C_ListResource(BaseListResource):
-    Meta = C_ResourceMeta
-
-
-class D_ResourceMeta:
-    service = D_Service
-    swagger = D_Swagger
-
-
-@setup_resource
-class D_DetailResource(BaseDetailResource):
-    Meta = D_ResourceMeta
-
-
-@setup_resource
-class D_ListResource(BaseListResource):
-    Meta = D_ResourceMeta
-
-
-class F_ResourceMeta:
-    service = F_Service
-    swagger = F_Swagger
-
-
-@setup_resource
-class F_DetailResource(BaseDetailResource):
-    Meta = F_ResourceMeta
-
-
-@setup_resource
-class F_ListResource(BaseListResource):
-    Meta = F_ResourceMeta
-
-
-class G_ResourceMeta:
-    service = G_Service
-    swagger = G_Swagger
-
-
-@setup_resource
-class G_ListResource(BaseListResource):
-    Meta = G_ResourceMeta
+class G_Resource(BaseResource):
+    class Meta:    
+        service = G_Service
+        swagger = G_Swagger
