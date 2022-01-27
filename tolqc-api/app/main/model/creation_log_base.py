e62dc10f0700768dfe96b62157bf66ad9a4421e4
Edward Moulsdale <em29@sanger.ac.uk>
2022-01-27 15:03:51 +0000

    Made changes

--- tolqc-api/app/main/model/creation_log_base.py
+++ tolqc-api/app/main/model/creation_log_base.py
@@ -27,11 +27,15 @@ class CreationLogMixin(object):
 
     @declared_attr
     def history(cls):
-        return db.Column(db.JSON, nullable=False, default={})
+        return db.Column(db.JSON, nullable=False, default=[])
 
 
 class CreationLogBase(Base, CreationLogMixin):
     __abstract__ = True
+
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+        self._create_history_array()
 
     @classmethod
     def has_creation_details(cls):
@@ -42,13 +46,31 @@ class CreationLogBase(Base, CreationLogMixin):
         #TODO test that this works in a CreationLogBase inherited test model
         if not hasattr(cls, 'Meta'):
             return []
-        return list(getattr(cls.Meta, 'exclude_columns_in_history', []))
+        return list(getattr(cls.Meta, 'exclude_columns_in_history', [])) + ['id']
 
     def post_update(self, user_id):
         #TODO check that datetime's don't break dumping of history
+        #maybe set a dump method that converts to string??
         self.last_modified_by = user_id
         self.last_modified_at = datetime.now()
 
     def post_create(self, user_id):
         self.created_by = user_id
         self.last_modified_by = user_id
+
+    def _get_history_entry(self):
+        return {}
+
+    def _create_history_array(self):
+        self.history = [self._get_history_entry()]
+
+    def _update_history(self):
+        self.history.append(self._get_history_entry())
+
+    def create(self, *args, **kwargs):
+        self._create_history_array()
+        super(Base).save(*args, **kwargs)
+
+    def update(self, *args, **kwargs):
+        self._update_history()
+        super(Base).update(*args, **kwargs)
