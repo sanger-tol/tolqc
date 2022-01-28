9265f82baa4e13a48ebaa7e8874b7f78f773ffe8
Edward Moulsdale <em29@sanger.ac.uk>
2022-01-28 14:02:08 +0000

    Test continues to pass

--- tolqc-api/app/test/base/tests/test_history_column_log_base.py
+++ tolqc-api/app/test/base/tests/test_history_column_log_base.py
@@ -55,3 +55,65 @@ class TestHistoryColumnLogBase(BaseTestCase):
                 }
             }
         )
+        #TODO test that history and other new features can't be hacked by specifying in request
+
+        # patch this to new state
+        response = self.client.open(
+            f'/api/v1/H/{instance_id}',
+            method='PATCH',
+            json={
+                "data": {
+                    "type": 'H',
+                    "attributes": {
+                        'string_column': 'how are you?'
+                    }
+                }
+            },
+            headers=self._get_api_key_2()
+        )
+        self.assert200(
+            response,
+            f'Response body is : {response.data.decode("utf-8")}'
+        )
+        # pull out unpredictable elements
+        last_modified_at_1 = response.json['data']['attributes']['last_modified_at']
+        # assert that history has one entry, and is correct
+        self.assertEqual(
+            response.json,
+            {
+                'data': {
+                    'type': 'H',
+                    'id': instance_id,
+                    'attributes': {
+                        'string_column': 'how are you?',
+                        'created_at': created_at,
+                        'last_modified_at': last_modified_at_1,
+                        'history': [{
+                            'string_column': 'hello there',
+                            'modified_at': None,
+                            'modified_by': None
+                        }]
+                    },
+                    'relationships': {
+                        'creator': {
+                            'data': {
+                                'id': '100',
+                                'type': 'users'
+                            },
+                            'links': {
+                                'related': '/users/100'
+                            }
+                        },
+                        'last_modifier': {
+                            'data': {
+                                'id': '101',
+                                'type': 'users'
+                            },
+                            'links': {
+                                'related': '/users/101'
+                            }
+                        }
+                    }
+                }
+            }
+        )        
