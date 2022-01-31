915c59cd98f558be316ee556f9bf228fd51072aa
Edward Moulsdale <em29@sanger.ac.uk>
2022-01-31 15:47:48 +0000

    EnumBase skeleton

---
+++ tolqc-api/app/main/model/enum_base.py
@@ -0,0 +1,15 @@
+# SPDX-FileCopyrightText: 2022 Genome Research Ltd.
+#
+# SPDX-License-Identifier: MIT
+
+from .base import Base, db
+
+
+class EnumBase(Base):
+    id = db.Column(db.Integer(), primary_key=True)
+    name = db.Column(db.String())
+    description = db.Column(db.String(), nullable=True)
+
+    @classmethod
+    def is_enum_table(cls):
+        return False
