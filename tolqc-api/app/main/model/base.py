39dd419c3dcde2326b4d87c31b8aba13baaa9406
Edward Moulsdale <em29@sanger.ac.uk>
2022-01-31 16:41:21 +0000

    Mistake

--- tolqc-api/app/main/model/base.py
+++ tolqc-api/app/main/model/base.py
@@ -302,10 +302,6 @@ class Base(db.Model):
         return False
 
     @classmethod
-    def is_enum_table(cls):
-        return False
-
-    @classmethod
     def get_foreign_key_column_names(cls):
         return [
             c.name for c in cls._get_columns()
