f09d6f2a5a680016c7d296109e118a5049f3c1f6
Edward Moulsdale <em29@sanger.ac.uk>
2022-02-01 13:44:02 +0000

    Added security test

--- tolqc-api/app/test/base/tests/test_enum_methods_by_name.py
+++ tolqc-api/app/test/base/tests/test_enum_methods_by_name.py
@@ -46,12 +46,47 @@ class TestEnumMethodsByName(BaseTestCase):
     def test_I_get_by_bad_name_404(self):
         # add an irrelevant I
         self.add_I(id=348523, name='nice')
-        # get a non-exsistant name
-        response_by_name = self.client.open(
+        # get a non-exsistent name
+        response = self.client.open(
             '/api/v1/I/name/not_nice',
             method='GET'
         )
         self.assert404(
-            response_by_name,
-            f'Response body is : {response_by_name.data.decode("utf-8")}'
+            response,
+            f'Response body is : {response.data.decode("utf-8")}'
+        )
+#TODO check if you can overwrite the name by name
+    def test_no_override_id_by_name_patch_400(self):
+        # add an I
+        self.add_I(id=4989, name='happy')
+        # attempt to patch to a new id
+        response = self.client.open(
+            '/api/v1/I/name/happy',
+            method='PATCH',
+            json={
+                'data': {
+                    'type': 'I',
+                    'id': '1337',
+                    'attributes': {
+                        'description': 'This is a hacker.'
+                    }
+                }
+            },
+            headers=self._get_api_key_1()
+        )
+        self.assert400(
+            response,
+            f'Response body is : {response.data.decode("utf-8")}'
+        )
+        # check it failed for the right reason
+        self.assertEqual(
+            response.json,
+            {
+                'errors': [{
+                    'detail': 'Unknown field.',
+                    'source': {
+                        'pointer': '/data/id'
+                    }
+                }]
+            }
         )
