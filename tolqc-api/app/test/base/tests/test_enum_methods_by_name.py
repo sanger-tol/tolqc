55d10fb2c852f2421751369fe9f2ec3c24826479
Edward Moulsdale <em29@sanger.ac.uk>
2022-02-01 13:32:17 +0000

    New test

--- tolqc-api/app/test/base/tests/test_enum_methods_by_name.py
+++ tolqc-api/app/test/base/tests/test_enum_methods_by_name.py
@@ -8,26 +8,26 @@ from test.base import BaseTestCase
 class TestEnumMethodsByName(BaseTestCase):
     def test_I_get_by_name_matches_by_id_200(self):
         self.add_I(id=348598, name='testing')
-        response = self.client.open(
-            '/api/v1/I',
-            method='POST',
-            json={
+        # get by id
+        response_by_id = self.client.open(
+            '/api/v1/I/348598',
+            method='GET'
+        )
+        self.assert200(
+            response_by_id,
+            f'Response body is : {response_by_id.data.decode("utf-8")}'
+        )
+        # assert that the response is correct
+        self.assertEqual(
+            response_by_id.json,
+            {
                 'data': {
-                    'id': 9999,
-                    'type': 'B',
-                },
-                "relationships": {
-                    "A": {
-                        "data": {
-                            "type": 'A',
-                            "id": 9090
-                        }
+                    'type': 'I',
+                    'id': '348598',
+                    'attributes': {
+                        'name': 'testing',
+                        'description': None
                     }
                 }
-            },
-            headers=self._get_api_key_1()
-        )
-        self.assert400(
-            response,
-            f'Response body is : {response.data.decode("utf-8")}'
-        )
+            }
+        )    
