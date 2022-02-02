e1ef41acd62c4577c387ece804b5d27c17a6da8f
Edward Moulsdale <em29@sanger.ac.uk>
2022-02-02 10:48:31 +0000

    New test now working

--- tolqc-api/app/test/base/tests/test_enum_swagger_json.py
+++ tolqc-api/app/test/base/tests/test_enum_swagger_json.py
@@ -2,6 +2,8 @@
 #
 # SPDX-License-Identifier: MIT
 
+from urllib.parse import unquote_plus
+
 from test.base import BaseTestCase
 
 
@@ -25,3 +27,27 @@ class TestEnumSwaggerJson(BaseTestCase):
         assert '/I/{id}' in paths
         assert '/I/name/{name}/J' in paths
         assert '/I/{id}/J' in paths
+
+    def _get_response_model(self, swagger_json, method, path, code):
+        method_dict = swagger_json['paths'][path][method]
+        response_ref = method_dict['responses'][code]['schema']['$ref']
+        response_model_name = unquote_plus(response_ref.split('/')[2])
+        return swagger_json['definitions'][response_model_name]
+
+    def test_enum_by_name_response_models_same_as_by_id_detail_get(self):
+        swagger_json = self._get_swagger_json_file()
+
+        response_model_by_name = self._get_response_model(
+            swagger_json,
+            'get',
+            '/I/name/{name}',
+            '200'
+        )
+        response_model_by_id = self._get_response_model(
+            swagger_json,
+            'get',
+            '/I/{id}',
+            '200'
+        )
+
+        self.assertEqual(response_model_by_id, response_model_by_name)
