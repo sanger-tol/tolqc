f2f31470231a19f46259e2519ccf5bb28b3f8587
Edward Moulsdale <em29@sanger.ac.uk>
2022-02-03 11:25:53 +0000

    Added another test

--- tolqc-api/app/test/base/tests/test_enum_link_relationship_by_name.py
+++ tolqc-api/app/test/base/tests/test_enum_link_relationship_by_name.py
@@ -7,6 +7,7 @@ from test.base import BaseTestCase
 
 class TestEnumLinkRelationshipByName(BaseTestCase):
     #TODO test the various breakage paths for specifying both name and id, neither name nor id, and name on non enum schema!
+    # also specifying non-existent name
     def test_post_J_specify_I_by_name(self):
         self.add_I(id=4857, name='nicely')
         response = self.client.open(
@@ -47,6 +48,53 @@ class TestEnumLinkRelationshipByName(BaseTestCase):
                             },
                             'links': {
                                 'related': '/I/4857'
+                            }
+                        }
+                    }
+                }
+            }
+        )
+
+    def test_post_J_specify_I_by_id(self):
+        self.add_I(id=3021349, name='also')
+        response = self.client.open(
+            '/api/v1/J',
+            method='POST',
+            headers=self._get_api_key_1(),
+            json={
+                'data': {
+                    'type': 'J',
+                    'relationships': {
+                        'I': {
+                            'data': {
+                                'type': 'I',
+                                'id': '3021349'
+                            }
+                        }
+                    }
+                }
+            }
+        )
+        self.assert201(response)
+
+        # get non predictable id
+        created_id = response.json['data']['id']
+
+        # assert response json is correct
+        self.assertEqual(
+            response.json,
+            {
+                'data': {
+                    'type': 'J',
+                    'id': created_id,
+                    'relationships': {
+                        'I': {
+                            'data': {
+                                'type': 'I',
+                                'id': '3021349'
+                            },
+                            'links': {
+                                'related': '/I/3021349'
                             }
                         }
                     }
