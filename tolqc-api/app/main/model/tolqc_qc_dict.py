22213abcab0e4acd194319027f6a0ca94f4e24f5
Edward Moulsdale <em29@sanger.ac.uk>
2022-02-07 14:41:09 +0000

    Converted dicts to BaseEnum

--- tolqc-api/app/main/model/tolqc_qc_dict.py
+++ tolqc-api/app/main/model/tolqc_qc_dict.py
@@ -2,15 +2,13 @@
 #
 # SPDX-License-Identifier: MIT
 
-from .base import Base, db, setup_model
+from .base import db, setup_model
+from .enum_base import EnumBase
 
 
 @setup_model
-class TolqcQcDict(Base):
+class TolqcQcDict(EnumBase):
     __tablename__ = "qc_dict"
-    id = db.Column(db.Integer(), primary_key=True)
-    name = db.Column(db.String())
-    description = db.Column(db.String())
     status = db.relationship("TolqcStatus", back_populates="qc_dict")
     genomescope_metrics = db.relationship("TolqcGenomescopeMetrics",
                                           back_populates="qc_dict")
