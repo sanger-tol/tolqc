292dba95951186a267c110d0238b2092fb3596d9
Edward Moulsdale <em29@sanger.ac.uk>
2022-02-08 14:29:37 +0000

    Far fewer test failing

--- tolqc-api/app/test/tolqc/test_user_security.py
+++ tolqc-api/app/test/tolqc/test_user_security.py
@@ -7,20 +7,20 @@ from main.model import TolqcAssemblyComponent
 from test.tolqc import TolqcTestCase
 
 
-class TestUserSecurity(TolqcTestCase):
-    def test_no_credential_disclosure_get_users(self):
-        expected_user = {
-            "type": "user",
+class TestusersSecurity(TolqcTestCase):
+    def test_no_credential_disclosure_get_userss(self):
+        expected_users = {
+            "type": "users",
             "attributes": {
-                "name": "test_user_admin",
-                "email": "test_user_admin@sanger.ac.uk",
+                "name": "test_users_admin",
+                "email": "test_users_admin@sanger.ac.uk",
                 "organisation": "Sanger Institute"
             },
             "id": "100",
             'relationships': {
-                'role': {
+                'roles': {
                     'links': {
-                        'related': '/user/100/role'
+                        'related': '/users/100/roles'
                     }
                 }
             }
@@ -28,7 +28,7 @@ class TestUserSecurity(TolqcTestCase):
 
         # assert no credential disclosure in list get
         response = self.client.open(
-            '/api/v1/user?filter=[name=="test_user_admin"]',
+            '/api/v1/users?filter=[name=="test_users_admin"]',
             method='GET'
         )
         self.assert200(
@@ -38,13 +38,13 @@ class TestUserSecurity(TolqcTestCase):
         self.assertEqual(
             response.json,
             {
-                "data": [expected_user]
+                "data": [expected_users]
             }
         )
 
         # assert no credential disclosure in detail get
         response = self.client.open(
-            '/api/v1/user/100',
+            '/api/v1/users/100',
             method='GET'
         )
         self.assert200(
@@ -54,17 +54,17 @@ class TestUserSecurity(TolqcTestCase):
         self.assertEqual(
             response.json,
             {
-                "data": expected_user
+                "data": expected_users
             }
         )
 
     def test_no_blind_equal_filter_credential_disclosure(self):
         """Ensures that a word-list attack, in which a hacker
         may guess an api_key from a list of likely candidates by
-        filtering against api_key value on list get users, is not
+        filtering against api_key value on list get userss, is not
         possible."""
         response = self.client.open(
-            f'/api/v1/user?filter=[api_key=="{self.api_key_1}"]',
+            f'/api/v1/users?filter=[api_key=="{self.api_key_1}"]',
             method='GET'
         )
         self.assert400(response)
@@ -82,7 +82,7 @@ class TestUserSecurity(TolqcTestCase):
         )
 
     def test_no_overwrite_creator_in_request(self):
-        """Ensures that an authenticated user can not defraud the
+        """Ensures that an authenticated users can not defraud the
         creator log by specifying another creator in a POST or
         PATCH request"""
         # N.B. this method needs an endpoint with a model inheriting
@@ -99,11 +99,11 @@ class TestUserSecurity(TolqcTestCase):
         }
 
         response = self.client.open(
-            '/api/v1/enum/assembly_component',
+            '/api/v1/enum/assembly_components',
             method='POST',
             json={
                 'data': {
-                    'type': 'assembly_component',
+                    'type': 'assembly_components',
                     'attributes': {
                         'name': 'component_no_1',
                         'description': 'this is a description'
@@ -111,7 +111,7 @@ class TestUserSecurity(TolqcTestCase):
                     'relationships': {
                         'creator': {
                             'data': {
-                                'type': 'user',
+                                'type': 'users',
                                 'id': '100'
                             }
                         }
@@ -131,28 +131,28 @@ class TestUserSecurity(TolqcTestCase):
 
         # try to modify creation details of existing assembly component instance
         # add the instance
-        assembly_component_instance = TolqcAssemblyComponent()
-        assembly_component_instance.id = 4321
-        assembly_component_instance.name = 'test'
-        assembly_component_instance.description = 'another test'
-        assembly_component_instance.created_by = 101
-        assembly_component_instance.last_modified_by = 101
-        assembly_component_instance.save_create(user_id=101)
+        assembly_components_instance = TolqcAssemblyComponent()
+        assembly_components_instance.id = 4321
+        assembly_components_instance.name = 'test'
+        assembly_components_instance.description = 'another test'
+        assembly_components_instance.created_by = 101
+        assembly_components_instance.last_modified_by = 101
+        assembly_components_instance.save_create(user_id=101)
 
         # try to modify its creator
         response = self.client.open(
-            '/api/v1/enum/assembly_component/test',
+            '/api/v1/enum/assembly_components/test',
             method='PATCH',
             json={
                 'data': {
-                    'type': 'assembly_component',
+                    'type': 'assembly_components',
                     'attributes': {
                         'description': 'this desc has now changed'
                     },
                     'relationships': {
                         'creator': {
                             'data': {
-                                'type': 'user',
+                                'type': 'users',
                                 'id': '100'
                             }
                         }
@@ -171,7 +171,7 @@ class TestUserSecurity(TolqcTestCase):
         self.assertEqual(response.json, expected_response)
 
     def test_no_overwrite_created_at_in_request(self):
-        """Ensures that an authenticated user can not defraud the
+        """Ensures that an authenticated users can not defraud the
         creation datetime log by specifying another value in a POST or
         PATCH request"""
         # N.B. this method needs an endpoint with a model inheriting
@@ -186,13 +186,12 @@ class TestUserSecurity(TolqcTestCase):
                 }
             ]
         }
-
         response = self.client.open(
-            '/api/v1/enum/assembly_component',
+            '/api/v1/enum/assembly_components',
             method='POST',
             json={
                 'data': {
-                    'type': 'assembly_component',
+                    'type': 'assembly_components',
                     'attributes': {
                         'name': 'component_no_1',
                         'description': 'this is a description',
@@ -213,21 +212,21 @@ class TestUserSecurity(TolqcTestCase):
 
         # try to modify created_at datetime of existing data instance
         # add the instance
-        assembly_component_instance = TolqcAssemblyComponent()
-        assembly_component_instance.id = 43
-        assembly_component_instance.name = 'test'
-        assembly_component_instance.description = 'another test'
-        assembly_component_instance.created_by = 101
-        assembly_component_instance.last_modified_by = 101
-        assembly_component_instance.save_create(user_id=101)
+        assembly_components_instance = TolqcAssemblyComponent()
+        assembly_components_instance.id = 43
+        assembly_components_instance.name = 'test'
+        assembly_components_instance.description = 'another test'
+        assembly_components_instance.created_by = 101
+        assembly_components_instance.last_modified_by = 101
+        assembly_components_instance.save_create(user_id=101)
 
         # try to modify its creator
         response = self.client.open(
-            '/api/v1/enum/assembly_component/test',
+            '/api/v1/enum/assembly_components/test',
             method='PATCH',
             json={
                 'data': {
-                    'type': 'assembly_component',
+                    'type': 'assembly_components',
                     'attributes': {
                         'created_at': str(datetime.now())
                     }
