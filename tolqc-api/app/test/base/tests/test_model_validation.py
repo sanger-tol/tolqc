aaa09f37f55910eddc15f6f646940988d1fdfcc9
Edward Moulsdale <em29@sanger.ac.uk>
2022-02-08 13:38:34 +0000

    Two new (failing) tests

--- tolqc-api/app/test/base/tests/test_model_validation.py
+++ tolqc-api/app/test/base/tests/test_model_validation.py
@@ -4,7 +4,8 @@
 
 import pytest
 
-from main.model.base import Base, db, setup_model
+from main.model.base import Base, db, setup_model, \
+                            ModelValidationError
 
 from test.base import BaseTestCase
 
@@ -22,3 +23,25 @@ class TestModelValidation(BaseTestCase):
             id = db.Column(db.Integer, primary_key=True)
         
         setup_model(GoodModel)
+
+    def test_model_missing_meta_class_fails(self):
+        class BadModel(Base):
+
+            __tablename__ = 'bad name'
+
+            id = db.Column(db.Integer, primary_key=True)
+        
+        with pytest.raises(ModelValidationError):
+            setup_model(BadModel)
+
+    def test_model_missing_type_fails(self):
+        class BadModel(Base):
+            class Meta:
+                pass
+
+            __tablename__ = 'bad name'
+
+            id = db.Column(db.Integer, primary_key=True)
+        
+        with pytest.raises(ModelValidationError):
+            setup_model(BadModel)
