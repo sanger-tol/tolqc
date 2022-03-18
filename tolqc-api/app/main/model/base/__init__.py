21d9391f9d01933efe486c76a3730ebe69019c49
Edward Moulsdale <em29@sanger.ac.uk>
2022-03-18 11:27:22 +0000

    Added ext fields mixin import

--- tolqc-api/app/main/model/base/__init__.py
+++ tolqc-api/app/main/model/base/__init__.py
@@ -3,7 +3,6 @@ from .base import Base, db, BadParameterException, \
                  StemInstanceDoesNotExistException, \
                  NamedEnumStemInstanceDoesNotExistException, \
                  ModelValidationError, setup_model
-
 from .enum_base import EnumBase, NamedEnumInstanceDoesNotExistException
-
 from .log_mixin import LogMixin
+from .ext_fields_mixin import ExtFieldsMixin
