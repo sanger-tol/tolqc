231d6705ec1485553c156e80ff89709bdfff3c2a
Edward Moulsdale <em29@sanger.ac.uk>
2022-05-20 10:45:14 +0100

    Ported work from Submissions

--- tolqc-api/app/run.py
+++ tolqc-api/app/run.py
@@ -5,12 +5,14 @@
 from main import application
 
 app = application()
-reload = True
 
 
 def main():
-    print("Starting ToLQC application...")
-    app.run(host='0.0.0.0', debug=True, port=80, use_reloader=reload)
+    app.run(
+        host='0.0.0.0',
+        debug=True,
+        port=80
+    )
 
 
 if __name__ == '__main__':
