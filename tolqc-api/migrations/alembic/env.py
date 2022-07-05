0d558466255636fdf4d131cada3c0d04a2eb8b50
Edward Moulsdale <em29@sanger.ac.uk>
2022-07-05 09:45:34 +0100

    Add autogenerated revision

--- tolqc-api/migrations/alembic/env.py
+++ tolqc-api/migrations/alembic/env.py
@@ -6,6 +6,8 @@ from sqlalchemy import create_engine
 
 from alembic import context
 
+from main.model import Base
+
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
@@ -21,7 +23,7 @@ if config.config_file_name is not None:
 # for 'autogenerate' support
 # from myapp import mymodel
 # target_metadata = mymodel.Base.metadata
-target_metadata = None
+target_metadata = [Base.metadata]
 
 # other values from the config, defined by the needs of env.py,
 # can be acquired:
