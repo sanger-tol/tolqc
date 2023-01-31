81af812b7b6edd3986a6d4130b767679fcbff092
Andrew Varley <av10@sanger.ac.uk>
2023-01-31 14:23:34 +0000

    Update SDK and UI versions. Add table to home page

--- tolqc-ui/app/src/pages/Home.tsx
+++ tolqc-ui/app/src/pages/Home.tsx
@@ -5,29 +5,14 @@ SPDX-License-Identifier: MIT
 */
 
 import React from "react";
-import { Header, HeaderButton, CentreContents } from '@tol/tol-ui'
-
-const apiButton: HeaderButton = {
-  href: 'api/v1/ui',
-  text: 'Use the API'
-};
-
-const UIButton: HeaderButton = {
-  href: 'api/v1/ui',
-  text: 'Use the UI'
-};
+import { AutoTable, CentreContents } from '@tol/tol-ui'
 
 function Home() {
   return (
     <div className="home">
-      <Header
-        title="TolQC"
-        sub_title="Tree of Life"
-        buttons={[apiButton]}
-      />
       <CentreContents>
-        <h2 className="display-4">What is TolQC?</h2>
-        <p>Currently a database/API for keeping track of data coming out of the sequencing pipeline. There is a separate graphical view, available from the link at the top of this page.</p>
+        <AutoTable endpoint="species"
+        />
       </CentreContents>
 
     </div>
