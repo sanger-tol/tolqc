9de2d0eac870f6da9d5cb7cdf7c182b21302f718
Kiernan Harding <kh16@sanger.ac.uk>
2023-03-01 16:13:32 +0000

    Add contexts

---
+++ tolqc-ui/app/src/contexts/auth.context.ts
@@ -0,0 +1,27 @@
+// SPDX-FileCopyrightText: 2022 Genome Research Ltd.
+//
+// SPDX-License-Identifier: MIT
+
+import { createContext, useContext } from 'react';
+import User from '../models/User'
+
+interface AuthContextValue {
+  token: string;
+  setToken: (token: string) => void;
+  user: User | null;
+  setUser: (user: User | null) => void;
+}
+
+export const AuthContext = createContext<AuthContextValue>({
+  token: '',
+  setToken() {
+    throw new Error('Missing AuthContext Provider');
+  },
+  user: null,
+  setUser() {
+    throw new Error('Missing AuthContext Provider');
+  },
+});
+
+export const AuthProvider = AuthContext.Provider;
+export const useAuth = () => useContext(AuthContext);
