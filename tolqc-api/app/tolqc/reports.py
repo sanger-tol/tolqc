8dc842bc65f0371c50c25e0f0a617a8d3e6ab14e
William Eagles <weaglesbio@gmail.com>
2025-01-03 15:38:14 +0000

    Add illumina report

--- tolqc-api/app/tolqc/reports.py
+++ tolqc-api/app/tolqc/reports.py
@@ -13,11 +13,13 @@ from sqlalchemy.orm import Bundle
 from tol.api_base2 import custom_blueprint
 
 from tolqc.schema.sample_data_models import (
+    Accession,
     Allocation,
     Data,
     File,
     Library,
     LibraryType,
+    MappingMetrics,
     PacbioRunMetrics,
     Platform,
     Project,
@@ -351,46 +353,31 @@ def illumina_data_report_query():
             Specimen.specimen_id.label('specimen'),
             Platform.name.label('platform'),
             Platform.model,
-            # Sample.sample_id.label('sample'),
-            #RUNID
-            #READ PAIRS
-            #YIELD
-
-            IsoDayBundle('date', Run.start),
-            # Data.lims_qc,
-            Run.lims_id.label('run'),
-            Run.run_id.label('movie_name'),
-            # Run.element.label('well'),
-            # Run.instrument_name.label('instrument'),
-            # Run.plex_count,
-            # PacbioRunMetrics.movie_minutes.label('movie_length'),
-            # Data.tag_index,
-            # Data.tag1_id.label('tag'),
+            Data.data_id.label('run'),
+            Data.reads.label('read_pairs'),
+            Data.bases.label('yield'),
+            MappingMetrics.average_quality.label('avg qual'),
+            Data.read_length_mean.label('avg length'),
             Sample.accession_id.label('sample_accession'),
             Data.accession_id.label('run_accession'),
+            Project.accession_id.label('study_accession'),
+            Accession.date_submitted.label('submission_date'),
+            Sample.sample_id.label('sanger_id'),
+            Data.tag1_id.label('tag_sequence'),
+            Data.tag2_id.label('tag2_sequence'),
+            Data.lims_qc.label('npg_qc_status'),
+            IsoDayBundle('date', Run.start),
             Species.species_id.label('species'),
-            #RUN STATUS
-            #DATE
-            # Data.library_id.label('library'),
-            # Data.reads,
-            # Data.read_length_mean,
-            # Data.read_length_n50,
-            # Data.read_length_longest,
-            # Data.read_length_shortest,
-            # Data.reads_duplicated,
-            # Data.reads_filtered,
-            # Data.bases.label('bases'),
-            # Data.bases_a,
-            # Data.bases_c,
-            # Data.bases_g,
-            # Data.bases_t,
-            # PacbioRunMetrics.loading_conc.label('loading_concentration'),
-            # PacbioRunMetrics.binding_kit,
-            # PacbioRunMetrics.sequencing_kit,
-            # PacbioRunMetrics.productive_zmws_num,
-            # PacbioRunMetrics.p0_num,
-            # PacbioRunMetrics.p1_num,
-            # PacbioRunMetrics.p2_num,
+            Library.library_type_id.label('pipeline_id_lims'),
+            Data.read_length_n50,
+            Data.read_length_longest,
+            Data.read_length_shortest,
+            Data.reads_duplicated,
+            Data.reads_filtered,
+            Data.bases_a,
+            Data.bases_c,
+            Data.bases_g,
+            Data.bases_t,
         )
         .select_from(Data)
         .outerjoin(Sample)
@@ -400,16 +387,18 @@ def illumina_data_report_query():
         .join(Platform)
         .outerjoin(Library)
         .outerjoin(LibraryType)
-        # .outerjoin(PacbioRunMetrics)
+        .outerjoin(MappingMetrics)
         # Cannot do many-to-many join between Data and Project directly.
         # Must explicitly go through Allocation:
         .join(Allocation)
         .join(Project)
+        # Join accession onto Data
+        .outerjoin(Data.accession)
         .where(Platform.name == 'Illumina')
         .order_by(
             Data.date.desc(),
             Specimen.specimen_id,
-        ).limit(5)
+        )
     )
     query = add_argument(query, Data.study_id)
     return query
@@ -467,3 +456,296 @@ class IsoDateTimeBundle(Bundle):
             return dt.isoformat() if dt else None
 
         return processor
+
+
+
+
+
+
+# "group":"jaron/nematodes",
+# "type":"hic-arima2",
+# "specimen":"nrCaeEleg1",
+# "platform":"Illumina",
+# "model":"NovaSeqX",
+# "run":"49103_3-4#8",
+# "reads_paired":1853705600,
+# "total_length":279909545600,
+# "average_quality":39,
+# "average_length":151,
+# "accession_number":"SAMEA13831494",
+# run_accession
+# exp_accession
+# study_accession
+# submission_date
+# "sanger_id":"JaronRG14807346",
+# "tag_sequence":"AGACACTA",
+# "tag2_sequence":"GTTTCCTA",
+# "run_qc_status":"qc complete",
+# "npg_qc_status":"1",
+# "date":"2024-08-09",
+# "species":"Caenorhabditis elegans",
+# "species_lims":"Caenorhabditis elegans",
+# "pipeline_id_lims":"Hi-C - Arima v2",
+# barcode
+
+
+#     {"reads_MQ0":0,
+#     "inward_oriented_pairs":0,
+#     "tag_index":"8",
+#     "library_id":"SQPP-57330-W:H2",
+#     "reads_properly_paired":0,
+#     "plot-2_hic":"jaron/nematodes/Caenorhabditis_elegans/genomic_data/nrCaeEleg1/hic-arima2/stats/49103_3-4#8/49103_3-4#8.hic.png",
+#     "reads_QC_failed":0,
+
+#     "plot-1_acgt-cycles":"jaron/nematodes/Caenorhabditis_elegans/genomic_data/nrCaeEleg1/hic-arima2/stats/49103_3-4#8/49103_3-4#8_F0xB00-acgt-cycles.png",
+#     "plot-1_quals3":"jaron/nematodes/Caenorhabditis_elegans/genomic_data/nrCaeEleg1/hic-arima2/stats/49103_3-4#8/49103_3-4#8_F0xB00-quals3.png",
+    
+
+#     "similarty":null,
+#     "plot-1_quals":"jaron/nematodes/Caenorhabditis_elegans/genomic_data/nrCaeEleg1/hic-arima2/stats/49103_3-4#8/49103_3-4#8_F0xB00-quals.png",
+#     "insert_size_average":0,
+#     "maximum_length":151,
+#     "reads_duplicated":0,
+#     "description":"",
+
+    
+#     "run_complete":"2024-07-02",
+#     "outward_oriented_pairs":0,
+#     "instrument":"NX2",
+#     "study_name":"ToL_Jaron_ReferenceGenomes_DNA",
+#     "species_name":"Caenorhabditis elegans",
+#     "plot-1_quals-hm":"jaron/nematodes/Caenorhabditis_elegans/genomic_data/nrCaeEleg1/hic-arima2/stats/49103_3-4#8/49103_3-4#8_F0xB00-quals-hm.png",
+#     "bases_duplicated":0,
+#     "bases_mapped_cigar":0,
+
+#     "insert_size_standard_deviation":0,
+
+#     "position":"",
+
+
+
+
+#     "raw_total_sequences":1853705600,
+
+#     "id_run":"49103",
+
+#     "plot-1_gc-content":"jaron/nematodes/Caenorhabditis_elegans/genomic_data/nrCaeEleg1/hic-arima2/stats/49103_3-4#8/49103_3-4#8_F0xB00-gc-content.png",
+#     "1st_fragments":926852800,
+#     "irods_path":"/seq/illumina/runs/49/49103/lane3-4/plex8",
+#     "study_id":"7382",
+#     "non_primary_alignments":0,
+#     "supplier_name":"SAN20000995",
+#     "mismatches":0,
+
+#     "plot-1_quals2":"jaron/nematodes/Caenorhabditis_elegans/genomic_data/nrCaeEleg1/hic-arima2/stats/49103_3-4#8/49103_3-4#8_F0xB00-quals2.png",
+#     "plot-2_fragment_separation":"jaron/nematodes/Caenorhabditis_elegans/genomic_data/nrCaeEleg1/hic-arima2/stats/49103_3-4#8/49103_3-4#8.fragment_separation.png",
+#     "reads_unmapped":1853705600,
+#     "reads_mapped":0,
+#     "taxonomicidentification":"Caenorhabditis elegans",
+#     "reads_mapped_and_paired":0,
+#     "irods_file":"49103_3-4#8.cram",
+#     "sequences":1853705600,
+
+#     "biospecimen_accession":"SAMEA13831490",
+#     "error_rate":0,
+#     "is_sorted":true,
+
+#     "pairs_on_different_chromosomes":0,
+#     "bases_mapped":0,
+
+#     "taxon_id":"6239",
+
+
+#     "filtered_sequences":0,
+#     "bases_trimmed":0,
+#     "pairs_with_other_orientation":0,
+
+#     "last_fragments":926852800
+#     }
+
+
+
+
+
+#     {"reads_MQ0":0,
+#     "inward_oriented_pairs":0,
+#     "tag_index":"8",
+#     "library_id":"SQPP-57330-W:H2",
+#     "reads_properly_paired":0,
+#     "plot-2_hic":"jaron/nematodes/Caenorhabditis_elegans/genomic_data/nrCaeEleg1/hic-arima2/stats/49103_3-4#8/49103_3-4#8.hic.png",
+#     "reads_QC_failed":0,
+#     "reads_paired":1853705600,
+#     "plot-1_acgt-cycles":"jaron/nematodes/Caenorhabditis_elegans/genomic_data/nrCaeEleg1/hic-arima2/stats/49103_3-4#8/49103_3-4#8_F0xB00-acgt-cycles.png",
+#     "plot-1_quals3":"jaron/nematodes/Caenorhabditis_elegans/genomic_data/nrCaeEleg1/hic-arima2/stats/49103_3-4#8/49103_3-4#8_F0xB00-quals3.png",
+#     "total_length":279909545600,
+#     "pipeline_id_lims":"Hi-C - Arima v2",
+#     "similarty":null,
+#     "plot-1_quals":"jaron/nematodes/Caenorhabditis_elegans/genomic_data/nrCaeEleg1/hic-arima2/stats/49103_3-4#8/49103_3-4#8_F0xB00-quals.png",
+#     "insert_size_average":0,
+#     "maximum_length":151,
+#     "reads_duplicated":0,
+#     "description":"",
+#     "sanger_id":"JaronRG14807346",
+#     "npg_qc_status":"1",
+#     "run_complete":"2024-07-02",
+#     "outward_oriented_pairs":0,
+#     "instrument":"NX2",
+#     "study_name":"ToL_Jaron_ReferenceGenomes_DNA",
+#     "species_name":"Caenorhabditis elegans",
+#     "plot-1_quals-hm":"jaron/nematodes/Caenorhabditis_elegans/genomic_data/nrCaeEleg1/hic-arima2/stats/49103_3-4#8/49103_3-4#8_F0xB00-quals-hm.png",
+#     "bases_duplicated":0,
+#     "bases_mapped_cigar":0,
+#     "average_length":151,
+#     "insert_size_standard_deviation":0,
+#     "run_qc_status":"qc complete",
+#     "position":"",
+#     "tag2_sequence":"GTTTCCTA",
+#     "run":"49103_3-4#8",
+#     "tag_sequence":"AGACACTA",
+#     "platform":"Illumina",
+#     "raw_total_sequences":1853705600,
+#     "type":"hic-arima2",
+#     "id_run":"49103",
+#     "group":"jaron/nematodes",
+#     "plot-1_gc-content":"jaron/nematodes/Caenorhabditis_elegans/genomic_data/nrCaeEleg1/hic-arima2/stats/49103_3-4#8/49103_3-4#8_F0xB00-gc-content.png",
+#     "1st_fragments":926852800,
+#     "irods_path":"/seq/illumina/runs/49/49103/lane3-4/plex8",
+#     "study_id":"7382","non_primary_alignments":0,
+#     "supplier_name":"SAN20000995","mismatches":0,
+#     "average_quality":39,
+#     "plot-1_quals2":"jaron/nematodes/Caenorhabditis_elegans/genomic_data/nrCaeEleg1/hic-arima2/stats/49103_3-4#8/49103_3-4#8_F0xB00-quals2.png",
+#     "plot-2_fragment_separation":"jaron/nematodes/Caenorhabditis_elegans/genomic_data/nrCaeEleg1/hic-arima2/stats/49103_3-4#8/49103_3-4#8.fragment_separation.png",
+#     "reads_unmapped":1853705600,
+#     "reads_mapped":0,
+#     "taxonomicidentification":"Caenorhabditis elegans",
+#     "reads_mapped_and_paired":0,
+#     "irods_file":"49103_3-4#8.cram",
+#     "sequences":1853705600,
+#     "species":"Caenorhabditis elegans",
+#     "biospecimen_accession":"SAMEA13831490",
+#     "error_rate":0,
+#     "is_sorted":true,
+#     "species_lims":"Caenorhabditis elegans",
+#     "pairs_on_different_chromosomes":0,
+#     "bases_mapped":0,
+#     "model":"NovaSeqX",
+#     "taxon_id":"6239",
+#     "accession_number":"SAMEA13831494",
+#     "date":"2024-08-09",
+#     "filtered_sequences":0,
+#     "bases_trimmed":0,
+#     "pairs_with_other_orientation":0,
+#     "specimen":"nrCaeEleg1",
+#     "last_fragments":926852800
+#     }
+
+
+
+#     {"mean":13600.4705464709,
+#     "plot-base_yield":["https://tolqc.cog.sanger.ac.uk/seq/pacbio/r64221e_20230519_150747/2_B01/reports/base_yield_plot.png","Base yield density"],
+#     "plot-concordance":["https://tolqc.cog.sanger.ac.uk/seq/pacbio/r64221e_20230519_150747/2_B01/reports/concordance_plot.png","Control concordance"],
+#     "pipeline_id_lims":"PacBio - HiFi",
+#     "largest":49385,
+#     "input":"jaron/nematodes/Caenorhabditis_elegans/genomic_data/nrCaeEleg2/pacbio/stats/m64221e_230520_201452.ccs.bc2003--bc2003.stats",
+#     "boldstats":{
+#         "taxon":"Caenorhabditis elegans","taxid":27260,
+#         "tax_rank":"species",
+#         "parentname":"Caenorhabditis",
+#         "stats":{
+#             "species":1,
+#             "barcodespecies":1,
+#             "barcodespecimens":21,
+#             "publicbins":1,"publicrecords":22,
+#             "sequencedspecimens":22,
+#             "publicspecies":1,
+#             "publicmarkersequences":{"COI-3P":1,"COI-5P":21},
+#             "specimenrecords":22
+#             },
+#             "tax_division":"Animalia",
+#             "parentid":27259
+#             },
+#     "plot-readlength":["https://tolqc.cog.sanger.ac.uk/seq/pacbio/r64221e_20230519_150747/2_B01/reports/readlength_plot.png","Control polymerase read length"],
+#     "plot-m5c_detections":["https://tolqc.cog.sanger.ac.uk/seq/pacbio/r64221e_20230519_150747/2_B01/reports/m5c_detections.png","CpG methylation in reads"],
+#     "plot-hexbin_length":["https://tolqc.cog.sanger.ac.uk/seq/pacbio/r64221e_20230519_150747/2_B01/reports/hexbin_length_plot.png","Insert read length density"],
+#     "tag_index":"2003",
+#     "c":17.9,
+#     "plot-readLenDist0":["https://tolqc.cog.sanger.ac.uk/seq/pacbio/r64221e_20230519_150747/2_B01/reports/readLenDist0.png","Polymerase read length"],
+#     "plot-ccs_hifi_read_length_yield":["https://tolqc.cog.sanger.ac.uk/seq/pacbio/r64221e_20230519_150747/2_B01/reports/ccs_hifi_read_length_yield_plot.png","HiFi yield by read length"],
+#     "sum":23025215822,
+#     "filtered_reads":7238,
+#     "study_name":"959 Nematode Genomes",
+#     "species_name":"Caenorhabditis elegans",
+#     "barcode":"bc2003--bc2003",
+#     "instrument":"m64221e",
+#     "plot-ccs_npasses_hist":["https://tolqc.cog.sanger.ac.uk/seq/pacbio/r64221e_20230519_150747/2_B01/reports/ccs_npasses_hist.png","Number of passes"],
+#     "sanger_id":"BlaxGeNe13447431",
+#     "filtered":0.42753217418835,
+#     "g":17.9,
+#     "plot-raw_read_length":["https://tolqc.cog.sanger.ac.uk/seq/pacbio/r64221e_20230519_150747/2_B01/reports/raw_read_length_plot.png","Loading evaluation"],
+#     "description":""
+#     "match":[
+#         {"specimen":{
+#             "url":"http://www.boldsystems.org/index.php/Public_RecordView?processid=GBNM1564-13",
+#             "collectionlocation":{"country":{},"coord":{"lon":{},"lat":{}}}
+#             },
+#         "taxonomicidentification":"Caenorhabditis elegans",
+#         "similarity":"1",
+#         "ID":"GBNM1564-13",
+#         "sequencedescription":"COI-5P",
+#         "database":"BOLD: Public Records","citation":"BOLD Systems, 2024"
+#         }],
+#     "plot-m5c_detections_hist":["https://tolqc.cog.sanger.ac.uk/seq/pacbio/r64221e_20230519_150747/2_B01/reports/m5c_detections_hist.png","CpG methylation in reads histogram"],
+#     "a":32.1,
+#     "L100":1692972,
+#     "L60":673531,
+#     "n":1692972,
+#     "dups":0,
+#     "supplier_name":"PD1074",
+#     "well_label":"B01",
+#     "L50":530778,
+#     "study_id":"6137",
+#     "smallest":100,
+#     "movie":"m64221e_230520_201452",
+#     "pipeline":"PacBio - HiFi",
+#     "group":"jaron/nematodes",
+#     "plot-readlength_qv_hist2d.hexbin":["https://tolqc.cog.sanger.ac.uk/seq/pacbio/r64221e_20230519_150747/2_B01/reports/readlength_qv_hist2d.hexbin.png","Accuracy versus read length density"],
+#     "N70":13336,
+#     "N80":10946,
+#     "type":"pacbio",
+#     "run":"TRACTION-RUN-598",
+#     "L90":1269196,
+#     "tag_sequence":"ACGAGTGCTCGAGTAT",
+#     "platform":"PacBio",
+#     "plot-ccs_accuracy_hist":["https://tolqc.cog.sanger.ac.uk/seq/pacbio/r64221e_20230519_150747/2_B01/reports/ccs_accuracy_hist.png","Read quality distribution"],
+#     "plot-ccs_all_readlength_hist":["https://tolqc.cog.sanger.ac.uk/seq/pacbio/r64221e_20230519_150747/2_B01/reports/ccs_all_readlength_hist_plot.png","Read length distribution (all)"],
+#     "specimen":"nrCaeEleg2",
+#     "N100":100,
+#     "N60":15280,
+#     "bases_a":7391948405,
+#     "accession_number":"SAMEA13831494",
+#     "taxon_id":"6239","date":"2023-05-20",
+#     "bases_t":7394194740,
+#     "bases_g":4116364115,
+#     "t":32.1,
+#     "N50":16993,
+#     "boldcheck":{
+#         "to":"6033",
+#         "nucleotides":"GGTGGTTTTGGTAACTGATTATTACCACTTATGTTAGGAGCACCTGATATAAGATTTCCACGTTTAAATAATTTAAGATTTTGGTTATTACCTACATCTATATTATTAATTTTAGATGCTTGTTTTGTAGATATAGGTTGTGGGACTAGGTGAACAGTCTACCCACCTTTAAGAACAATGGGGCACCCTGGAAGTAGAGTAGATTTAGCTATTTTTAGTTTACATGCAGCAGGGTTAAGATCTATCTTAGGTGGTATTAATTTTATGTGTACTACTAAAAATTTACGTAGAAGTTCTATTTCATTAGAACATATAACTTTATTTGTTTGAACTGTGTTTGTAACAGTGTTTTTACTGGTTTTATCTCTACCGGTTTTAGCAGGGGCTATTACTATGTTGTTAACTGATCGTAATTTAAATACTTCATTTTTTGATCCAAGAACTGGAGGTAATCCTCTTATTTATCAACATTTGTTTTGATTTTTTGGTCATCCTGAAGTATATATTTTGATTTTACCAGCTTTTGGTATTGTCAGACAATCTACACTTTATTTAACAGGAAAAAAAGAAGTTTTTGGTGCTTTGGGTATAGTTTATGCAATTTTAAGAATTGGTTTAATTGGTTGTGTAGTATGAGCTCACCATATGTATACAGTAGGTATAGATTTGGATTCACGTGCTTATTTT",
+#         "from":"5347",
+#         "readname":"m64221e_230520_201452/10158278/ccs",
+#         "strand":"+"},
+#     "model":"Sequel IIe",
+#     "bases_c":4122708562,
+#     "plot-ccs_readlength_hist":["https://tolqc.cog.sanger.ac.uk/seq/pacbio/r64221e_20230519_150747/2_B01/reports/ccs_readlength_hist_plot.png","HiFi read length distribution"],
+#     "species_lims":"Caenorhabditis elegans",
+#     "L70":834406,
+#     "rundir":"/seq/pacbio/r64221e_20230519_150747/2_B01",
+#     "species":"Caenorhabditis elegans",
+#     "dup_reads":0,
+#     "plot-interAdapterDist0":["https://tolqc.cog.sanger.ac.uk/seq/pacbio/r64221e_20230519_150747/2_B01/reports/interAdapterDist0.png","Adapter distribution"],
+#     "biospecimen_accession":"SAMEA13831490",
+#     "L80":1023934,
+#     "N90":7844,
+#     "irods_file":"demultiplex.bc2003--bc2003.bam",
+#     "library_load_name":""
+#     }
