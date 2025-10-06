a8b5c2774795ff0577c0e94f083f74eeb3af7a90
James Gilbert <jgrg@sanger.ac.uk>
2025-10-06 21:19:00 +0100

    pyproject.toml and associated changes

--- tolqc-api/app/run.py
+++ tolqc-api/app/run.py
@@ -9,21 +9,14 @@ from tol.sql import create_session_factory
 from tolqc.flask import application
 
 
-session_factory = create_session_factory(
-    os.environ['DB_URI']
-)
+session_factory = create_session_factory(os.environ['DB_URI'])
 
 
-app = application(
-    session_factory=session_factory
-)
+app = application(session_factory=session_factory)
 
 
 def main():
-    app.run(
-        host='0.0.0.0',
-        port=80
-    )
+    app.run(host='0.0.0.0', port=80)
 
 
 if __name__ == '__main__':
