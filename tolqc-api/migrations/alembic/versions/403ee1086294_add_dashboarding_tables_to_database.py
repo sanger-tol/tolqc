e6bc930a1b538007b4864087a7c417d6d0f2f5ae
William Eagles <weaglesbio@gmail.com>
2025-10-14 13:28:49 +0100

    Update database for authentication and customisation

---
+++ tolqc-api/migrations/alembic/versions/403ee1086294_add_dashboarding_tables_to_database.py
@@ -0,0 +1,117 @@
+"""add_dashboarding_tables_to_database
+
+Revision ID: 403ee1086294
+Revises: 81bf8897c7c6
+Create Date: 2025-10-13 13:08:39.428033
+
+"""
+from alembic import op
+import sqlalchemy as sa
+from sqlalchemy.dialects.postgresql import JSONB
+
+
+# revision identifiers, used by Alembic.
+revision = '403ee1086294'
+down_revision = '81bf8897c7c6'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    # Create table `component`
+    op.create_table(
+        'component',
+        sa.Column('id', sa.String, primary_key=True),
+        sa.Column('title', sa.String, nullable=False),
+        sa.Column('object_type', sa.String, nullable=False),
+        sa.Column('base_url', sa.String, nullable=True),
+        sa.Column('component_type', sa.String, nullable=False),
+        sa.Column('widget_type', sa.String, nullable=False),
+        sa.Column('config', JSONB, nullable=False),
+        sa.Column('filter', JSONB, nullable=False, default={}, server_default='{}'),
+        sa.Column('user_id', sa.Integer, sa.ForeignKey('user.id'), nullable=False)
+    )
+
+    # Create table `zone`
+    op.create_table(
+        'zone',
+        sa.Column('id', sa.String, primary_key=True),
+        sa.Column('title', sa.String, nullable=False),
+        sa.Column('object_type', sa.String, nullable=False),
+        sa.Column('base_url', sa.String, nullable=True),
+        sa.Column('filter', JSONB, nullable=False, default={}, server_default='{}'),
+        sa.Column('user_id', sa.Integer, sa.ForeignKey('user.id'), nullable=False)
+    )
+
+    # Create table `view`
+    op.create_table(
+        'view',
+        sa.Column('id', sa.String, primary_key=True),
+        sa.Column('title', sa.String, nullable=False),
+        sa.Column('filter', JSONB, nullable=False, default={}, server_default='{}'),
+        sa.Column('user_id', sa.Integer, sa.ForeignKey('user.id'), nullable=False)
+    )
+
+    # Create table `board`
+    op.create_table(
+        'board',
+        sa.Column('id', sa.String, primary_key=True),
+        sa.Column('title', sa.String, nullable=False),
+        sa.Column('filter', JSONB, nullable=False, default={}, server_default='{}'),
+        sa.Column('user_id', sa.Integer, sa.ForeignKey('user.id'), nullable=False)
+    )
+
+    # Create table `component_zone`
+    op.create_table(
+        'component_zone',
+        sa.Column('id', sa.Integer, primary_key=True, autoincrement=True),
+        sa.Column('order', sa.Integer, nullable=False),
+        sa.Column('component_id', sa.String, sa.ForeignKey('component.id'), nullable=False),
+        sa.Column('zone_id', sa.String, sa.ForeignKey('zone.id'), nullable=False)
+    )
+
+    # Create table `zone_view`
+    op.create_table(
+        'zone_view',
+        sa.Column('id', sa.Integer, primary_key=True, autoincrement=True),
+        sa.Column('order', sa.Integer, nullable=False),
+        sa.Column('zone_id', sa.String, sa.ForeignKey('zone.id'), nullable=False),
+        sa.Column('view_id', sa.String, sa.ForeignKey('view.id'), nullable=False)
+    )
+
+    # Create table `view_board`
+    op.create_table(
+        'view_board',
+        sa.Column('id', sa.Integer, primary_key=True, autoincrement=True),
+        sa.Column('order', sa.Integer, nullable=False),
+        sa.Column('view_id', sa.String, sa.ForeignKey('view.id'), nullable=False),
+        sa.Column('board_id', sa.String, sa.ForeignKey('board.id'), nullable=False)
+    )
+
+    # add order uniqueness constraints
+    op.create_unique_constraint(
+        None,
+        'component_zone',
+        ['zone_id', 'order']
+    )
+    op.create_unique_constraint(
+        None,
+        'zone_view',
+        ['view_id', 'order']
+    )
+    op.create_unique_constraint(
+        None,
+        'view_board',
+        ['board_id', 'order']
+    )
+
+
+def downgrade():
+    # Drop tables in reverse order of creation
+    op.drop_table('board')
+    op.drop_table('view_board')
+    op.drop_table('view')
+    op.drop_table('zone_view')
+    op.drop_table('zone')
+    op.drop_table('component_zone')
+    op.drop_table('component')
---
+++ tolqc-api/migrations/alembic/versions/81bf8897c7c6_add_oidc_tables.py
@@ -0,0 +1,99 @@
+"""update_user_table
+
+Revision ID: 81bf8897c7c6
+Revises: c8babd30b9f1
+Create Date: 2025-10-13 13:04:28.183333
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '81bf8897c7c6'
+down_revision = 'c8babd30b9f1'
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+
+    # Create oidc_state table
+    op.create_table(
+        'oidc_state',
+        sa.Column('id', sa.String, primary_key=True),
+        sa.Column('created_at', sa.DateTime, nullable=False),
+    )
+    # Create role_binding table
+    op.create_table(
+        'role_binding',
+        sa.Column('id', sa.Integer, primary_key=True, autoincrement=True),
+        sa.Column('user_id', sa.Integer, nullable=False),
+        sa.Column('role_id', sa.Integer, nullable=False),
+        sa.ForeignKeyConstraint(
+            ('user_id',),
+            ['user.id'],
+        ),
+        sa.ForeignKeyConstraint(
+            ('role_id',),
+            ['role.id'],
+        ),
+    )
+
+    # Update user table
+    op.drop_constraint('user_email_key', 'user')
+    op.alter_column('user', 'email', new_column_name='oidc_id')
+    op.create_unique_constraint(None, 'user', ['oidc_id'])
+    op.drop_column('user', 'name')
+    op.drop_column('user', 'organisation')
+    op.drop_column('user', 'registered')
+
+    # Update role table
+    op.add_column('role', sa.Column('name', sa.String, unique=True, nullable=False))
+    op.drop_constraint('role_user_id_fkey', 'role')
+    op.drop_column('role', 'user_id')
+
+    # Update token table
+    op.add_column('token', sa.Column('created_at', sa.DateTime, nullable=True))
+    op.add_column('token', sa.Column('expires_at', sa.DateTime, nullable=True))
+    for sql in [
+        """
+        UPDATE token
+        SET created_at = CURRENT_TIMESTAMP
+        WHERE created_at IS NULL
+        """,
+        """
+        UPDATE token
+        SET expires_at = '2030-01-01 00:00:01'
+        WHERE expires_at IS NULL
+        """
+    ]:
+        op.execute(sa.text(sql))
+    op.alter_column('token', 'created_at', nullable=False)
+    op.alter_column('token', 'expires_at', nullable=False)
+
+def downgrade() -> None:
+
+    # Drop oidc_state table
+    op.drop_table('oidc_state')
+
+    # Drop role_binding table
+    op.drop_table('role_binding')
+
+    # Revert user table changes
+    op.drop_constraint('user_oidc_id_key', table_name='user')
+    op.alter_column('user', 'oidc_id', new_column_name='email')
+    op.create_unique_constraint(None, 'user', ['email'])
+    op.add_column('user', sa.Column('name', sa.String, nullable=False))
+    op.add_column('user', sa.Column('organisation', sa.String, nullable=True))
+    op.add_column('user', sa.Column('registered', sa.Boolean, nullable=False, server_default=sa.boolean(False)))
+
+    # Revert role table changes
+    op.drop_column('role', 'name')
+    op.add_column('role', sa.Column('user_id', sa.Integer, nullable=True))
+    op.create_foreign_key('role_user_id_fkey', 'role', 'user', ['user_id'], ['id'])
+
+    # Revert token table changes
+    op.drop_column('token', 'created_at')
+    op.drop_column('token', 'expires_at')
+    
