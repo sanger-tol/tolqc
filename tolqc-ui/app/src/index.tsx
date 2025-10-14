2974b30f6df95b58ffdebc6330f3d92c530a5fc7
William Eagles <weaglesbio@gmail.com>
2025-10-14 15:51:30 +0100

    Update tolqc-ui, to standard default

--- tolqc-ui/app/src/index.tsx
+++ tolqc-ui/app/src/index.tsx
@@ -4,20 +4,33 @@ SPDX-FileCopyrightText: 2022 Genome Research Ltd.
 SPDX-License-Identifier: MIT
 */
 
-import ReactDOM from 'react-dom';
-import { Home } from './pages';
+import { createRoot } from 'react-dom/client';
+import { 
+  Home 
+} from './pages';
 import reportWebVitals from './reportWebVitals';
-import { TolApp } from '@tol/tol-ui'
+import { TolApp, Page, Dropdown, TsDataSource } from '@tol/tol-ui'
+import Logo from './assets/logo.png';
 import './scss/styling.scss';
 
+export const ELASTIC_DS = new TsDataSource({apiPrefix: "data/tol_production"});
+export const TOLQC_DS = new TsDataSource({apiPrefix: "data/tolqc"});
 
-ReactDOM.render(
+const root = createRoot(document.getElementById('root')!);
+root.render(
   <TolApp
-    brand='TolQC'
+    boards={{dataSource: TOLQC_DS}}
+    brand={
+      <img
+        src={Logo}
+        alt="ToL Portal Logo"
+        style={{height: 30}}
+      />
+    }
     homePage={<Home />}
     pages={[]}
-  />,
-  document.getElementById('root')
+    login={true}
+  />
 );
 
 // If you want to start measuring performance in your app, pass a function
