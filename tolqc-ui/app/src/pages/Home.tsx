74742754ecc5071b75d45380112aff5c644e6fb4
Nithin <nj9@sanger.ac.uk>
2026-02-12 09:01:03 +0000

    changes to check in the review app

--- tolqc-ui/app/src/pages/Home.tsx
+++ tolqc-ui/app/src/pages/Home.tsx
@@ -4,80 +4,67 @@ SPDX-FileCopyrightText: 2022 Genome Research Ltd.
 SPDX-License-Identifier: MIT
 */
 
-import { 
-  RemoteTable, 
-  Widgets, 
-  Button, 
-  Row, 
-  Col
-} from '@tol/tol-ui';
-import { ELASTIC_DS } from '..';
-
+import { RemoteTable, Widgets, Button, Row, Col } from "@tol/tol-ui";
 
 const button = (
-	<Button
-		href="https://portal.tol.sanger.ac.uk"
-		style={{float: "right"}}
-	>
-		Visit ToL Portal
-	</Button>
-)
+  <Button href="https://portal.tol.sanger.ac.uk" style={{ float: "right" }}>
+    Visit ToL Portal
+  </Button>
+);
 
 const title = (
-	<span>
-		<p className='mt-2'>
-			Welcome to ToLQC. The preferred place to view ToLQC data is in the ToL Portal.
-		</p>
-	</span>
-)
+  <span>
+    <p className="mt-2">
+      Welcome to ToLQC. The preferred place to view ToLQC data is in the ToL
+      Portal.
+    </p>
+  </span>
+);
 
 const intro = (
-	<Row>
-		<Col xs={12} sm={8}>{title}</Col>
-		<Col xs={12} sm={4}>{button}</Col>
-	</Row>
-)
+  <Row>
+    <Col xs={12} sm={8}>
+      {title}
+    </Col>
+    <Col xs={12} sm={4}>
+      {button}
+    </Col>
+  </Row>
+);
 
 function Home() {
   const dataTable = (
     <RemoteTable
       id="data-home-table-v2"
-      endpoint="data"
+      displaySource
       height={500}
       fields={{
-        data:{
-          "id": {
-            rename: "ID"
+        data: {
+          id: {
+            rename: "ID",
           },
-          "date": {
-            rename: "Date"
+          date: {
+            rename: "Date",
           },
-          "name": {
-            rename: "Name"
+          name: {
+            rename: "Name",
           },
           "sample.id": {
             rename: "Sanger Sample ID",
-            relationshipBox: true
-          }
+            relationshipBox: true,
+          },
         },
-        order:{
-          active:[
-            "id",
-            "date",
-            "name",
-            "sample.id"
-          ]
-        }
+        order: {
+          active: ["id", "date", "name", "sample.id"],
+        },
       }}
     />
   );
 
   return (
     <div className="data">
-      <Widgets components={[intro]}/>
-      <Widgets components={[
-				dataTable
-			]}/>
+      <Widgets components={[intro]} />
+      <Widgets components={[dataTable]} />
     </div>
   );
 }
