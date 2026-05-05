54be1ef1e2064707687d61cc2ce5d4be6aef0e45
James Gilbert <jgrg@sanger.ac.uk>
2026-05-05 11:19:19 +0100

    TOLA-617: Fix file.id sequence and updates to run table

--- tolqc-api/app/tolqc/schema/sample_data_models.py
+++ tolqc-api/app/tolqc/schema/sample_data_models.py
@@ -287,7 +287,7 @@ class Location(Base):
     path = mapped_column(String, index=True)
 
     species = relationship('Species', back_populates='location')
-    specimen = relationship('Specimen', back_populates='location')
+    specimens = relationship('Specimen', back_populates='location')
 
 
 class MappingMetrics(Base, HasFolder):
@@ -452,11 +452,9 @@ class Run(Base):
         return 'run_id'
 
     run_id = mapped_column(String, primary_key=True)
-    name = mapped_column(String)
-    hierarchy_name = mapped_column(String)
     platform_id = mapped_column(Integer, ForeignKey('platform.id'))
     centre_id = mapped_column(Integer, ForeignKey('centre.id'))
-    lims_id = mapped_column(String)
+    lims_id = mapped_column(String, index=True)
     element = mapped_column(String)
     instrument_name = mapped_column(String)
     start = mapped_column(DateTime(timezone=True))
@@ -580,7 +578,7 @@ class Specimen(LogBase):
         back_populates='specimen',
     )
 
-    location = relationship('Location', back_populates='specimen')
+    location = relationship('Location', back_populates='specimens')
 
     parent_assn = relationship(
         'Offspring',
---
+++ tolqc-api/migrations/alembic/versions/2acc2f8257a0_fix_file_id_sequence_value.py
@@ -0,0 +1,48 @@
+"""Fix file.id sequence value
+
+Revision ID: 2acc2f8257a0
+Revises: b35d5e13936f
+Create Date: 2026-04-09 14:49:48.254577
+
+"""
+
+from alembic import op
+from sqlalchemy import text
+from sqlalchemy.schema import CreateSequence, DropSequence, Sequence
+
+
+# revision identifiers, used by Alembic.
+revision = '2acc2f8257a0'
+down_revision = 'b35d5e13936f'
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    # Remove the old sequence
+    op.alter_column('file', 'id', server_default=None)
+    op.execute(DropSequence(Sequence('_alembic_tmp_file_id_seq')))
+
+    # Create the new sequence
+    seq = 'file_id_seq'
+    op.execute(CreateSequence(Sequence(seq)))
+    op.alter_column(
+        'file',
+        'id',
+        server_default=text(f"nextval('{seq}'::regclass)"),
+    )
+
+    # Set sequence to correct current value
+    set_sql = text(f"""
+      SELECT setval('{seq}', ( SELECT MAX(id) FROM file) )
+    """)  # noqa: S608
+    op.execute(set_sql)
+
+    # Patches to Run model
+    op.drop_column('run', 'name')
+    op.drop_column('run', 'hierarchy_name')
+    op.create_index(None, 'run', ['lims_id'], unique=False)
+
+
+def downgrade() -> None:
+    pass
