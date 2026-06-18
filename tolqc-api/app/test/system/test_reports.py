b0647b8b560712b3bcea4818fb2899458d150e40
James Gilbert <jgrg@sanger.ac.uk>
2026-06-18 18:01:55 +0100

    Test data and test for ena-assembly-data report

--- tolqc-api/app/test/system/test_reports.py
+++ tolqc-api/app/test/system/test_reports.py
@@ -25,13 +25,7 @@ def pacbio_row_count(db_session):
     To avoid hard coding a number, query for the number of PacBio data rows in
     the test database.
     """
-    n = (
-        db_session.query(Platform)
-        .join(Run)
-        .join(Data)
-        .where(Platform.name == 'PacBio')
-        .count()
-    )
+    n = db_session.query(Platform).join(Run).join(Data).where(Platform.name == 'PacBio').count()
 
     # Guard against tests being run on an empty database
     if not n > 1:
@@ -292,11 +286,23 @@ def test_ena_assembly_report(client, api_path):
     assert len(miss_gca) > 0
 
 
+def test_ena_assembly_data_report(client, api_path):
+    asm_data = report_response_json(
+        client,
+        api_path,
+        'ena-assembly-data',
+        {
+            'format': 'NDJSON',
+        },
+    )
+    assert len(asm_data) > 0
+    has_assemblies = [x for x in asm_data if x["assemblies"] is not None]
+    assert len(has_assemblies) > 0
+
+
 @pytest.fixture
 def max_bases(client, api_path):
-    json_lines = report_response_json(
-        client, api_path, 'specimen-status', {'format': 'NDJSON'}
-    )
+    json_lines = report_response_json(client, api_path, 'specimen-status', {'format': 'NDJSON'})
     assert json_lines
     return sum_species_data_bases(json_lines)
 
@@ -351,7 +357,5 @@ def expected_values(params):
 
 
 def test_data_report_bad_params(client, api_path):
-    response = client.get(
-        api_path + '/report/pipeline-data?' + urlencode({'processed': 'x'})
-    )
+    response = client.get(api_path + '/report/pipeline-data?' + urlencode({'processed': 'x'}))
     assert response.status == '500 INTERNAL SERVER ERROR'
