7140f12f70c758465143d1bd78ea7b4f9b6dfe63
James Gilbert <jgrg@sanger.ac.uk>
2026-06-19 11:51:49 +0100

    ENA controlled vocab columns for library_type table

--- tolqc-api/migrations/alembic/versions/127e17ed1743_assembly_category_and_library_is_pcr.py
+++ tolqc-api/migrations/alembic/versions/127e17ed1743_assembly_category_and_library_is_pcr.py
@@ -115,7 +115,9 @@ def upgrade() -> None:
                 nullable=True,
             ),
         )
-    op.execute(sa.text('UPDATE library_type SET reporting_category_new = reporting_category'))
+    op.execute(
+        sa.text('UPDATE library_type SET reporting_category_new = reporting_category')
+    )
     op.execute(
         sa.text("""
           UPDATE library_type
@@ -128,7 +130,9 @@ def upgrade() -> None:
         """)
     )
     op.drop_column('library_type', 'reporting_category')
-    op.alter_column('library_type', 'reporting_category_new', new_column_name='reporting_category')
+    op.alter_column(
+        'library_type', 'reporting_category_new', new_column_name='reporting_category'
+    )
     op.create_foreign_key(
         None,
         'library',
@@ -147,6 +151,145 @@ def upgrade() -> None:
         'library_type',
         ['is_pcr'],
         unique=False,
+    )
+
+    # New ENA controlled vocabulary columns
+    op.create_table(
+        'library_selection_dict',
+        sa.Column('selection', sa.String(), nullable=False),
+        sa.PrimaryKeyConstraint('selection'),
+    )
+    op.create_table(
+        'library_source_dict',
+        sa.Column('source', sa.String(), nullable=False),
+        sa.PrimaryKeyConstraint('source'),
+    )
+    op.create_table(
+        'library_strategy_dict',
+        sa.Column('strategy', sa.String(), nullable=False),
+        sa.PrimaryKeyConstraint('strategy'),
+    )
+
+    op.add_column('library', sa.Column('description', sa.String(), nullable=True))
+
+    # Three ENA controlled vocab columns
+    op.add_column(
+        'library_type',
+        sa.Column('source', sa.String(), nullable=True),
+    )
+    op.add_column(
+        'library_type',
+        sa.Column('selection', sa.String(), nullable=True),
+    )
+    op.add_column(
+        'library_type',
+        sa.Column('strategy', sa.String(), nullable=True),
+    )
+
+    # Three ENA controlled vocab foreign keys
+    op.create_foreign_key(
+        None, 'library_type', 'library_source_dict', ['source'], ['source']
+    )
+    op.create_foreign_key(
+        None, 'library_type', 'library_selection_dict', ['selection'], ['selection']
+    )
+    op.create_foreign_key(
+        None, 'library_type', 'library_strategy_dict', ['strategy'], ['strategy']
+    )
+
+    op.add_column(
+        'library_type', sa.Column('description_template', sa.String(), nullable=True)
+    )
+
+    # Drop foreign key constraint for all tables pointing at project
+    project_rel_tables = (
+        'allocation',
+        'edit_project',
+    )
+    for tbl in project_rel_tables:
+        op.drop_constraint(f'{tbl}_project_id_fkey', tbl)
+    op.drop_constraint('study_default_project_id_fkey', 'study')
+
+    # Batch alter table to place columns in desired order
+    with op.batch_alter_table('project', recreate='always') as batch_op:
+        batch_op.add_column(
+            sa.Column('sts_alias', sa.String(), nullable=True),
+            insert_after='project_id',
+        )
+    op.create_index(
+        op.f('ix_project_sts_alias'), 'project', ['sts_alias'], unique=False
+    )
+
+    # Recreate project table foreign key constraints
+    for tbl in project_rel_tables:
+        op.create_foreign_key(
+            None,
+            tbl,
+            'project',
+            ['project_id'],
+            ['project_id'],
+        )
+    op.create_foreign_key(
+        None,
+        'study',
+        'project',
+        ['default_project_id'],
+        ['project_id'],
+    )
+
+    # Drop foreign key constraint for all tables pointing at specimen
+    specimen_rel_tables = (
+        'assembly',
+        'edit_specimen',
+        'offspring',
+        'sample',
+        'specimen_status',
+    )
+    for tbl in specimen_rel_tables:
+        op.drop_constraint(f'{tbl}_specimen_id_fkey', tbl)
+    op.drop_constraint('offspring_offspring_specimen_id_fkey', 'offspring')
+    op.drop_constraint('metagenome_host_specimen_id_fkey', 'metagenome')
+    op.drop_constraint('specimen_cobiont_specimen_id_fkey', 'specimen')
+
+    # Batch alter table to place columns in desired order
+    with op.batch_alter_table('specimen', recreate='always') as batch_op:
+        batch_op.add_column(
+            sa.Column('priority', sa.Integer(), nullable=True),
+            insert_after='sts_priority',
+        )
+    op.create_index(
+        op.f('ix_specimen_priority'), 'specimen', ['priority'], unique=False
+    )
+
+    # Recreate specimen table foreign key constraints
+    for tbl in specimen_rel_tables:
+        op.create_foreign_key(
+            None,
+            tbl,
+            'specimen',
+            ['specimen_id'],
+            ['specimen_id'],
+        )
+    op.create_foreign_key(
+        None,
+        'offspring',
+        'specimen',
+        ['offspring_specimen_id'],
+        ['specimen_id'],
+    )
+    op.create_foreign_key(
+        None,
+        'metagenome',
+        'specimen',
+        ['host_specimen_id'],
+        ['specimen_id'],
+    )
+    op.create_foreign_key(
+        None,
+        'specimen',
+        'specimen',
+        ['cobiont_specimen_id'],
+        ['specimen_id'],
     )
 
 
